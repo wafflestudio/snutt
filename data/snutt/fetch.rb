cdbecb54d3ef53ce4b4fa43734708de762127931
zxzl <hyeungshikjung@Hyeungui-Mac-Pro.local>
2014-07-16 08:58:52 +0900

    fetching방식 수정
    1.영역별로 엑셀파일을 긁어온 후, 하나의 텍스트 파일에 합치는 형태로 바꿈. 텍스트파일의 각 라인의 끝에
    foundation_math 식으로 영역이 표시됨 (data/snutt/fetch.rb line22~)
    
    2. 엑셀파일이 많아져서 xls폴더안에 year_semester 폴더를 만듬. fileutils를 쓰래서 일단 썼는데 안 쓰는
    방법있으면 추천좀..
    
    3. 엑셀파일 불러오는 요청에서 맨 앞 srchCond 값을 1로 바꿈(상세검색)
    
    4. snuev 연동 흔적 일단 삭제

--- data/snutt/fetch.rb
+++ data/snutt/fetch.rb
@@ -2,99 +2,59 @@
 
 #수강편람을 긁어옴
 #인자 : year semester
+
 require 'net/http'
 require 'roo'
 require 'json'
+require 'fileutils'
 
 if ARGV.length != 2 then
-	puts "Argument error !"
-	puts "usage example : ruby fetch.rb 2012 S"
-	exit!
+    puts "Argument error !"
+    puts "usage example : ruby fetch.rb 2012 S"
+    exit!
 end
 year = ARGV[0]
 semester = ARGV[1] #1/S/2/W
 
 if !(year.to_i > 2000) then
-	puts "First argument should be year"
-	exit!
+    puts "First argument should be year"
+    exit!
 elsif !["1", "2", "S", "W"].include?(semester) then
-	puts "Second argument should be in [1, 2, S, W]"
-	exit!
+    puts "Second argument should be in [1, 2, S, W]"
+    exit!
 end
 
-category_map = {} #"xxx.xxx;yyy => "학문의 기초""
-
-#htm file download (wget)
-`sh gethtm.sh #{"#{year} #{semester}"}`
-Dir["htm/#{year}_#{semester}*.htm"].each do |htmfile|
-	result = `ruby parsing_htm.rb #{"#{htmfile}"}`.split("\n")
-	#학문의 기초 - ALL
-	if htmfile.include?("basics_all") then
-		result.each{|x| category_map[x] = "basics" unless category_map[x]}
-	#학문의 기초 - 외국어와 외국문화
-	elsif htmfile.include?("basics_foreign") then
-		result.each{|x| category_map[x] = "basics_foreign"}
-	#학문의 기초 - 국어와 작문
-	elsif htmfile.include?("basics_korean") then
-		result.each{|x| category_map[x] = "basics_korean"}
-	#학문의 기초 - 기초과학
-	elsif htmfile.include?("basics_science") then
-		result.each{|x| category_map[x] = "basics_science"}
-
-	#핵심교양 - 문학과 예술
-	elsif htmfile.include?("core_art") then
-		result.each{|x| category_map[x] = "core_art"}
-	#핵심교양 - 생명과 환경
-	elsif htmfile.include?("core_biology") then
-		result.each{|x| category_map[x] = "core_biology"}
-	#핵심교양 - 역사와 철학
-	elsif htmfile.include?("core_history") then
-		result.each{|x| category_map[x] = "core_history"}
-	#핵심교양 - 자연의 이해
-	elsif htmfile.include?("core_nature") then
-		result.each{|x| category_map[x] = "core_nature"}
-	#핵심교양 - 사회와 이념
-	elsif htmfile.include?("core_society") then
-		result.each{|x| category_map[x] = "core_society"}
-	#핵심교양 - 자연과 기술
-	elsif htmfile.include?("core_technology") then
-		result.each{|x| category_map[x] = "core_technology"}
-
-	#일반교양 - 문학과 예술
-	elsif htmfile.include?("normal_art") then
-		result.each{|x| category_map[x] = "normal_art"}
-	#일반교양 - 체육 및 기타
-	elsif htmfile.include?("normal_exercise") then
-		result.each{|x| category_map[x] = "normal_exercise"}
-	#일반교양 - 외국어와 외국문화
-	elsif htmfile.include?("normal_foreign") then
-		result.each{|x| category_map[x] = "normal_foreign"}
-	#일반교양 - 역사와 철학
-	elsif htmfile.include?("normal_history") then
-		result.each{|x| category_map[x] = "normal_history"}
-	#일반교양 - 국어와 작문
-	elsif htmfile.include?("normal_korean") then
-		result.each{|x| category_map[x] = "normal_korean"}
-	#일반교양 - 자연의 이해
-	elsif htmfile.include?("normal_nature") then
-		result.each{|x| category_map[x] = "normal_nature"}
-	#일반교양 - 기초과학
-	elsif htmfile.include?("normal_science") then
-		result.each{|x| category_map[x] = "normal_science"}
-	#일반교양 - 사회와 이념
-	elsif htmfile.include?("normal_society") then
-		result.each{|x| category_map[x] = "normal_society"}
-	#일반교양 - 기초교육 특별프로그램
-	elsif htmfile.include?("normal_special") then
-		result.each{|x| category_map[x] = "normal_special"}
-	end
-end
-puts "category mapping complete"
+#교과구분 [srchOpenSubmattFgCd, srchOpenSbjtFldCd, (자작)영문코드, 한국이름]
+fields = [
+["A", "40", "foundation_thinking", "학문의 기초: 사고와 표현"],
+["A", "41", "foundation_foreign", "학문의 기초: 외국어"],
+["A", "42", "foundation_math", "학문의 기초: 수량적 분석과 추론"],
+["A", "43", "foundation_science", "학문의 기초: 과학적 사고와 실험"],
+["A", "44", "foundation_computer", "학문의 기초: 컴퓨터와 정보 활용"],
+["A", "45", "world_literature", "학문의 세계: 언어와 문학"],
+["A", "46", "world_culture", "학문의 세계: 문화와 예술"],
+["A", "47", "world_history", "학문의 세계: 역사와 철학"],
+["A", "48", "world_politics", "학문의 세계: 정치와 경제"],
+["A", "49", "world_human", "학문의 세계: 인간과 사회"],
+["A", "50", "world_nature", "학문의 세계: 자연과 기술"],
+["A", "51", "world_life", "학문의 세계: 생명과 환경"],
+["A", "52", "elective_physical", "선택교양: 체육"],
+["A", "53", "elective_art", "선택교양: 예술 실기"],
+["A", "54", "elective_leadership", "선택교양: 대학과 리더십"],
+["A", "55", "elective_creativity", "선택교양: 창의와 융합"],
+["A", "56", "elective_korea", "선택교양: 한국의 이해"],
+["B", "", "requisite", "전필"],
+["C", "", "elective", "전선"],
+["D", "", "general", "일선"],
+["E", "", "teaching", "교직"],
+["F", "", "research", "논문"],
+["G", "", "graduate", "대학원"]
+]
 
 #download 
 puts "Start fetching...#{year}/#{semester}"
 
-xls_filename="#{Dir.getwd()}/xls/#{year}_#{semester}.xls"
+FileUtils.mkdir_p "xls/#{year}_#{semester}"
 txt_filename="#{Dir.getwd()}/txt/#{year}_#{semester}.txt"
 
 http = Net::HTTP.new('sugang.snu.ac.kr', 80)
@@ -109,19 +69,23 @@ when 'S'
 when 'W'
   shtm = 'U000200002U000300002'
 end
-data = "srchCond=0&pageNo=1&workType=EX&sortKey=&sortOrder=&srchOpenSchyy=#{year}&currSchyy=#{year}&srchOpenShtm=#{shtm}&srchCptnCorsFg=&srchOpenShyr=&srchSbjtCd=&srchSbjtNm=&srchOpenUpSbjtFldCd=&srchOpenSbjtFldCd=&srchOpenUpDeptCd=&srchOpenDeptCd=&srchOpenSubmattFgCd=&srchOpenPntMin=&srchOpenPntMax=&srchCamp=&srchBdNo=&srchProfNm=&srchTlsnAplyCapaCntMin=&srchTlsnAplyCapaCntMax=&srchTlsnRcntMin=&srchTlsnRcntMax=&srchOpenSbjtTmNm=&srchOpenSbjtTm=&srchOpenSbjtTmVal=&srchLsnProgType=&srchMrksGvMthd="
-res, data = http.post(path, data)
 
-open(xls_filename,"w") do |file|
-	file.print(res.body)
+fields.each do |field|
+    category = field[0]
+    fieldCode = field[1]
+    fieldNameEng = field[2]
+
+    data = "srchCond=1&pageNo=1&workType=EX&sortKey=&sortOrder=&srchOpenSchyy=#{year}&currSchyy=#{year}&srchOpenShtm=#{shtm}&srchCptnCorsFg=&srchOpenShyr=&srchSbjtCd=&srchSbjtNm=&srchOpenUpSbjtFldCd=&srchOpenSbjtFldCd=#{fieldCode}&srchOpenUpDeptCd=&srchOpenDeptCd=&srchOpenSubmattFgCd=#{category}&srchOpenPntMin=&srchOpenPntMax=&srchCamp=&srchBdNo=&srchProfNm=&srchTlsnAplyCapaCntMin=&srchTlsnAplyCapaCntMax=&srchTlsnRcntMin=&srchTlsnRcntMax=&srchOpenSbjtTmNm=&srchOpenSbjtTm=&srchOpenSbjtTmVal=&srchLsnProgType=&srchMrksGvMthd="
+    res, data = http.post(path, data)
+    
+    xls_filename="#{Dir.getwd()}/xls/#{year}_#{semester}/#{year}_#{semester}_#{fieldNameEng}.xls"
+    open(xls_filename,"w") do |file|
+    file.print(res.body)
+        puts "download complete : #{year}_#{semester}_#{fieldNameEng}.xls"
+    end
 end
-puts "download complete : #{year}_#{semester}.xls"
-
+puts "download complete, start converting"
 #convert
-puts "start converting from xls to txt"
-excel = Roo::Excel.new(xls_filename);
-m = excel.to_matrix
-
 def convert_classtime(time)
   wday = time[0]
   begin
@@ -148,58 +112,53 @@ def convert_classtime(time)
 end
 
 open("#{txt_filename}.tmp", "w") do |file|
-	file.puts "#{year}/#{semester}"
-	file.puts Time.now.localtime().strftime("%Y-%m-%d %H:%M:%S")
-	file.puts "classification;department;academic_year;course_number;lecture_number;course_title;credit;class_time;location;instructor;quota;enrollment;remark;category;snuev_lec_id;snuev_eval_score"
-	3.upto(m.row_size-1) do |i|
-		classification = m[i,0]
-		department = m[i,1]
-		academic_year = m[i,2]
-    if academic_year == "학사"
-      academic_year = m[i,3]
+    file.puts "#{year}/#{semester}"
+    file.puts Time.now.localtime().strftime("%Y-%m-%d %H:%M:%S")
+    file.puts "classification;department;academic_year;course_number;lecture_number;course_title;credit;class_time;location;instructor;quota;enrollment;remark;category;snuev_lec_id;snuev_eval_score"
+    
+    fields.each do |field|
+        fieldNameEng = field[2]
+        xls_filename="#{Dir.getwd()}/xls/#{year}_#{semester}/#{year}_#{semester}_#{fieldNameEng}.xls"
+        begin
+            excel = Roo::Excel.new(xls_filename)
+            m = excel.to_matrix
+
+            3.upto(m.row_size-1) do |i|
+                classification = m[i,0]
+                department = m[i,1]
+                academic_year = m[i,2]
+                if academic_year == "학사"
+                    academic_year = m[i,3]
+                end
+                    course_number = m[i,4]
+                    lecture_number = m[i,5]
+                    course_title = m[i,6]
+                if m[i,7].to_s.length > 1
+                    course_title = course_title + "(#{m[i,7]})"
+                end
+                course_title = course_title.gsub(/;/, ':')
+                credit = m[i,8].to_i
+                class_time = m[i,11]
+                location = m[i,13]
+                instructor = m[i,14]
+                quota = m[i,15].to_i
+                enrollment = m[i,16].to_i
+                remark = m[i,17].gsub(/\n/, " ")
+                category = fieldNameEng
+                snuev_lec_id = snuev_eval_score = nil
+            #classtime 표기 통일
+            #수(7,8,9) -> 수(7-3)
+                class_time = class_time.split("/").map{|x| convert_classtime(x)}.join("/")
+
+                file.puts "#{classification};#{department};#{academic_year};#{course_number};#{lecture_number};#{course_title};#{credit};#{class_time};#{location};#{instructor};#{quota};#{enrollment};#{remark};#{category};#{snuev_lec_id};#{snuev_eval_score}"
+            end
+            puts "converted_#{fieldNameEng}"
+        rescue
+            puts "empty_#{fieldNameEng}"
+        end     
     end
-		course_number = m[i,4]
-		lecture_number = m[i,5]
-		course_title = m[i,6]
-    if m[i,7].to_s.length > 1
-      course_title = course_title + "(#{m[i,7]})"
-    end
-    course_title = course_title.gsub(/;/, ':')
-		credit = m[i,8].to_i
-		class_time = m[i,11]
-		location = m[i,13]
-		instructor = m[i,14]
-		quota = m[i,15].to_i
-		enrollment = m[i,16].to_i
-		remark = m[i,17].gsub(/\n/, " ")
-		category = category_map["#{course_number};#{lecture_number}"]
-
-=begin
-		while true
-			snuev_api = nil
-			begin
-				snuev_api = JSON.parse(Net::HTTP.get(URI.parse(URI.escape("http://snuev.com/lecture/find?code=#{course_number}&professor=#{instructor}"))))
-			rescue
-				puts "fail"
-				snuev_api = nil
-			end
-			if snuev_api then
-				break
-			end
-		end
-		snuev_lec_id = snuev_api["lec_id"]
-		snuev_eval_score = (snuev_api["eval_point"].to_f / snuev_api["eval_count"].to_f).round(2)
-		puts "(#{i-2}/#{m.row_size-3}) #{course_number} #{instructor} #{course_title} : #{snuev_lec_id} : #{snuev_eval_score}"
-=end
-		snuev_lec_id = snuev_eval_score = nil
-
-		#classtime 표기 통일
-		#수(7,8,9) -> 수(7-3)
-		class_time = class_time.split("/").map{|x| convert_classtime(x)}.join("/")
-
-		file.puts "#{classification};#{department};#{academic_year};#{course_number};#{lecture_number};#{course_title};#{credit};#{class_time};#{location};#{instructor};#{quota};#{enrollment};#{remark};#{category};#{snuev_lec_id};#{snuev_eval_score}"
-	end
 end
+puts "------converting complete--------"
 
 File.rename("#{txt_filename}.tmp", txt_filename)
 
