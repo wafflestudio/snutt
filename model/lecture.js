ef70e9d5413d410730ad1031cdbba7fb50605ae1
Hyeungshik Jung <hyungshike@gmail.com>
2016-01-06 21:49:43 +0900

    convert tab to space :wink:

--- model/lecture.js
+++ model/lecture.js
@@ -2,23 +2,23 @@ var mongoose = require('mongoose');
 
 var LectureSchema = mongoose.Schema({
   year: { type: Number, required: true },
-	semester: { type: String, required: true },
-	classification: { type: String, required: true },
-	department: String,
-	academic_year: String,
-	course_number: { type: String, required: true },
-	lecture_number: String,
-	course_title: { type: String, required: true },
-	credit: Number,
-	class_time: String,
-	class_time_json: [
-	{ day : Number, start: Number, len: Number, place : String }
-	],
-	instructor: String,
-	quota: Number,
-	enrollment: Number,
-	remark: String,
-	category: String
+  semester: { type: String, required: true },
+  classification: { type: String, required: true },
+  department: String,
+  academic_year: String,
+  course_number: { type: String, required: true },
+  lecture_number: String,
+  course_title: { type: String, required: true },
+  credit: Number,
+  class_time: String,
+  class_time_json: [
+  { day : Number, start: Number, len: Number, place : String }
+  ],
+  instructor: String,
+  quota: Number,
+  enrollment: Number,
+  remark: String,
+  category: String
 });
 
 module.exports = mongoose.model('Lecture', LectureSchema);
