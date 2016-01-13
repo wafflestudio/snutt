da1ed24fd0b0cc1e3f8b0ae222ab766d91ef08e9
rajin9601 <rajin9601@naver.com>
2016-01-13 15:09:55 +0900

    init user for only debug

--- routes/routes.js
+++ routes/routes.js
@@ -2,7 +2,9 @@ var express = require('express');
 var router = express.Router();
 
 var apiRouter = require('./api/api.js');
+var initRouter = require('./init'); //FOR DEBUG ONLY, REMOVE THIS LINE BEFORE DEPLOY
 
 router.use('/api', apiRouter);
+router.use('/init', initRouter); //FOR DEBUG ONLY, REMOVE THIS LINE BEFORE DEPLOY
 
 module.exports = router;
