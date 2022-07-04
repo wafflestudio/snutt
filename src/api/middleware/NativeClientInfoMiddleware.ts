602c9286e17fbb20406cbcd83c775c57ef9ccbc9
davin111 <bdv111@snu.ac.kr>
2022-07-04 22:34:59 +0900

    check os and app versions

--- src/api/middleware/NativeClientInfoMiddleware.ts
+++ src/api/middleware/NativeClientInfoMiddleware.ts
@@ -3,6 +3,8 @@ import ErrorCode from "../enum/ErrorCode";
 import ApiError from "../error/ApiError";
 
 export default async function(req, res) {
+    const versionRegex = /^(\d+\.)?(\d+\.)?(\*|\d+)$/;
+
     const osType = <string>req.headers['x-os-type'];
     const osVersion = <string>req.headers['x-os-version'];
     const appType = <string>req.headers['x-app-type'];
@@ -11,7 +13,7 @@ export default async function(req, res) {
     let context: RequestContext = req['context'];
 
     if (osType !== undefined) {
-        if (osType === 'ios' || osType === 'android') {
+    if (osType === 'ios' || osType === 'android') {
             context.osType = osType;
         } else {
             throw new ApiError(400, ErrorCode.INVALID_OS_TYPE, `Invalid os type: ${osType}`);
@@ -19,7 +21,11 @@ export default async function(req, res) {
     }
 
     if (osVersion !== undefined) {
-        context.osVersion = osVersion;
+        if (osVersion.match(versionRegex)) {
+            context.osVersion = osVersion;
+        } else {
+            throw new ApiError(400, ErrorCode.INVALID_OS_VERSION, `Invalid os version: ${osVersion}`);
+        }
     }
 
     if (appType !== undefined) {
@@ -31,7 +37,11 @@ export default async function(req, res) {
     }
 
     if (appVersion !== undefined) {
-        context.appVersion = appVersion;
+        if (appVersion.match(versionRegex)) {
+            context.appVersion = appVersion;
+        } else {
+            throw new ApiError(400, ErrorCode.INVALID_APP_VERSION, `Invalid app version: ${appVersion}`);
+        }
     }
 
     return Promise.resolve('next');
