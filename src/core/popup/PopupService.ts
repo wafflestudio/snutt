cbb1a4c82ff62fddcb20a83019170a3fd2d8009c
Davin Byeon (Dan) <bdv111@snu.ac.kr>
2022-09-15 02:32:50 +0900

    Update PopupService.ts

--- src/core/popup/PopupService.ts
+++ src/core/popup/PopupService.ts
@@ -5,7 +5,7 @@ export async function getPopups(user: User, osType?: string, osVersion?: string,
     content: [
       {
         "key": "snutt-ev-open",
-        "image_url": "https://snutt-asset.s3.ap-northeast-2.amazonaws.com/popup-images/C30C86B0-5905-4A57-B1A4-218C6D1169B6.jpeg",
+        "image_url": "https://snutt-asset.s3.ap-northeast-2.amazonaws.com/popup-images/snutt-ev-open.png",
         "hidden_days": 1,
       },
     ],
