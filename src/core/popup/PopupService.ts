064a2f82814a07e3f527df514cdef557bcd78694
Davin Byeon (Dan) <bdv111@snu.ac.kr>
2022-11-04 23:02:40 +0900

    Update PopupService.ts

--- src/core/popup/PopupService.ts
+++ src/core/popup/PopupService.ts
@@ -2,6 +2,17 @@ import User from '@app/core/user/model/User';
 
 export async function getPopups(user: User, osType?: string, osVersion?: string, appType?: string, appVersion?: string) {
   return {
-    content: [],
+    content: [
+       {
+         "key": `test-popup-key-null (osType:${osType}/osVersion:${osVersion}/appType:${appType}/appVersion:${appVersion})`,
+         "image_url": "https://avatars.githubusercontent.com/u/35535636",
+         "hidden_days": null,
+       },
+       {
+         "key": `test-popup-key-1day (osType:${osType}/osVersion:${osVersion}/appType:${appType}/appVersion:${appVersion})`,
+         "image_url": "https://avatars.githubusercontent.com/u/48513130",
+         "hidden_days": 1,
+       },
+     ],
   };
 }
