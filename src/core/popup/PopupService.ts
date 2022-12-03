8aedfed2259b44317873a4aa77432c21631b2955
Davin Byeon (Dan) <bdv111@snu.ac.kr>
2022-12-04 08:35:07 +0900

    Revert "Revert "Revert "Update PopupService.ts (#236)" (#238)" (#240)"
    This reverts commit f337ee45562611e6c653020426b5e647179b1e15.

--- src/core/popup/PopupService.ts
+++ src/core/popup/PopupService.ts
@@ -2,17 +2,6 @@ import User from '@app/core/user/model/User';
 
 export async function getPopups(user: User, osType?: string, osVersion?: string, appType?: string, appVersion?: string) {
   return {
-    content: [
-       {
-         "key": `test-popup-key-null (osType:${osType}/osVersion:${osVersion}/appType:${appType}/appVersion:${appVersion})`,
-         "image_url": "https://avatars.githubusercontent.com/u/35535636",
-         "hidden_days": null,
-       },
-       {
-         "key": `test-popup-key-1day (osType:${osType}/osVersion:${osVersion}/appType:${appType}/appVersion:${appVersion})`,
-         "image_url": "https://avatars.githubusercontent.com/u/48513130",
-         "hidden_days": 1,
-       },
-     ],
+    content: [],
   };
 }
