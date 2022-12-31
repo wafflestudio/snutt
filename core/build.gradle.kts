cda071f6769fb76df482397d0788d7434d905153
jon <lgock2023@gmail.com>
2022-12-31 22:54:27 +0900

    setup embedded mongodb

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -1,2 +1,4 @@
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+    testRuntimeOnly("de.flapdoodle.embed:de.flapdoodle.embed.mongo.spring30x:4.3.2")
 }
