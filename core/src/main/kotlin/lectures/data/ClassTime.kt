b79d91fddf6f7765fb053092b43f8132b5157e2c
Hank-Choi <zlzlqlzl1@naver.com>
2023-01-20 21:21:20 +0900

    ClassTime 객체 수정

--- core/src/main/kotlin/lectures/data/ClassTime.kt
+++ core/src/main/kotlin/lectures/data/ClassTime.kt
@@ -1,8 +1,12 @@
 package com.wafflestudio.snu4t.lectures.data
 
+import com.fasterxml.jackson.annotation.JsonProperty
+import com.fasterxml.jackson.databind.PropertyNamingStrategies
+import com.fasterxml.jackson.databind.annotation.JsonNaming
 import org.springframework.data.annotation.Id
 import org.springframework.data.mongodb.core.mapping.Field
 
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class ClassTime(
     @Id
     var id: String,
@@ -13,7 +17,9 @@ data class ClassTime(
     @Field("end_time")
     var endTime: String,
     @Field("len")
+    @JsonProperty("len")
     var length: Double,
     @Field("start")
+    @JsonProperty("start")
     var startPeriod: Double,
 )
