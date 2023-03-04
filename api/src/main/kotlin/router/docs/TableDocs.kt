c45f1f81c1b614423687ae44109b819267667d2c
강지혁 <wlgur7238@snu.ac.kr>
2023-03-04 15:24:07 +0900

    api docs (swagger) 설정
    springdoc gradle dependency update
    asciidoctor 의존성 제거
    
    RouterOperations 적용
    Docs 정의 개별 파일로 분리 + RouterFunction 분리
    
    ++
    
    1. v1 prefix nesting 코드 때문에 람다 뎁스가 길어지는 것
    2. coRouter로 감싸져 있는데 타입 선언해서 길어지는 것
    
    두 개를 줄여보면 어떨까 싶어 줄여봤습니다

---
+++ api/src/main/kotlin/router/docs/TableDocs.kt
@@ -0,0 +1,25 @@
+package com.wafflestudio.snu4t.router.docs
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
+import io.swagger.v3.oas.annotations.enums.ParameterIn
+import io.swagger.v3.oas.annotations.media.Content
+import io.swagger.v3.oas.annotations.media.Schema
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import org.springdoc.core.annotations.RouterOperation
+import org.springdoc.core.annotations.RouterOperations
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.RequestMethod
+import timetables.dto.TimetableBriefDto
+
+@RouterOperations(
+    RouterOperation(
+        path = "/v1/tables", method = [RequestMethod.GET], produces = [MediaType.APPLICATION_JSON_VALUE],
+        operation = Operation(
+            operationId = "getBrief",
+            parameters = [Parameter(`in` = ParameterIn.HEADER, name = "userId", required = true)],
+            responses = [ApiResponse(responseCode = "200", content = [Content(schema = Schema(implementation = TimetableBriefDto::class))])]
+        ),
+    )
+)
+annotation class TableDocs
