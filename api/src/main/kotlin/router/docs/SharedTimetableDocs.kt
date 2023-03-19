32b6b648764aa2e20a7185918756b7a23e03f3d3
Subeen Park <psb021414@gmail.com>
2023-03-19 21:39:22 +0900

    docs lint

--- api/src/main/kotlin/router/docs/SharedTimetableDocs.kt
+++ api/src/main/kotlin/router/docs/SharedTimetableDocs.kt
@@ -1,14 +1,12 @@
 package com.wafflestudio.snu4t.router.docs
 
 import com.wafflestudio.snu4t.bookmark.dto.BookmarkLectureModifyRequest
-import com.wafflestudio.snu4t.bookmark.dto.BookmarkResponse
 import com.wafflestudio.snu4t.sharedtimetable.dto.SharedTimetableBriefDto
 import com.wafflestudio.snu4t.sharedtimetable.dto.SharedTimetableCreateRequest
 import com.wafflestudio.snu4t.sharedtimetable.dto.SharedTimetableModifyRequest
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.enums.ParameterIn
-import io.swagger.v3.oas.annotations.media.ArraySchema
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.parameters.RequestBody
@@ -64,8 +62,10 @@ import org.springframework.web.bind.annotation.RequestMethod
         path = "/v1/shared-tables/", method = [RequestMethod.DELETE], produces = [MediaType.APPLICATION_JSON_VALUE],
         operation = Operation(
             operationId = "deleteSharedTimetable",
-            parameters = [Parameter(`in` = ParameterIn.HEADER, name = "userId", required = true),
-                         Parameter(`in` = ParameterIn.PATH, name = "timetableId", required = true)],
+            parameters = [
+                Parameter(`in` = ParameterIn.HEADER, name = "userId", required = true),
+                Parameter(`in` = ParameterIn.PATH, name = "timetableId", required = true)
+            ],
             requestBody = RequestBody(content = [Content(schema = Schema(implementation = BookmarkLectureModifyRequest::class))]),
             responses = [ApiResponse(responseCode = "200", content = [Content(schema = Schema())])]
         ),
