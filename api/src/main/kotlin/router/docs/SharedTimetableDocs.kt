2fc5fe494965c708824644de06a5314a82f0ad9b
Subeen Park <psb021414@gmail.com>
2023-03-19 21:53:33 +0900

    swagger docs array schema 수정

--- api/src/main/kotlin/router/docs/SharedTimetableDocs.kt
+++ api/src/main/kotlin/router/docs/SharedTimetableDocs.kt
@@ -1,12 +1,13 @@
 package com.wafflestudio.snu4t.router.docs
 
-import com.wafflestudio.snu4t.bookmark.dto.BookmarkLectureModifyRequest
 import com.wafflestudio.snu4t.sharedtimetable.dto.SharedTimetableBriefDto
 import com.wafflestudio.snu4t.sharedtimetable.dto.SharedTimetableCreateRequest
+import com.wafflestudio.snu4t.sharedtimetable.dto.SharedTimetableDetailDto
 import com.wafflestudio.snu4t.sharedtimetable.dto.SharedTimetableModifyRequest
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.enums.ParameterIn
+import io.swagger.v3.oas.annotations.media.ArraySchema
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.parameters.RequestBody
@@ -24,7 +25,7 @@ import org.springframework.web.bind.annotation.RequestMethod
             parameters = [
                 Parameter(`in` = ParameterIn.HEADER, name = "userId", required = true),
             ],
-            responses = [ApiResponse(responseCode = "200", content = [Content(schema = Schema(implementation = SharedTimetableBriefDto::class))])]
+            responses = [ApiResponse(responseCode = "200", content = [Content(array = ArraySchema(schema = Schema(implementation = SharedTimetableBriefDto::class)))])]
         ),
     ),
     RouterOperation(
@@ -35,7 +36,7 @@ import org.springframework.web.bind.annotation.RequestMethod
                 Parameter(`in` = ParameterIn.HEADER, name = "userId", required = true),
                 Parameter(`in` = ParameterIn.PATH, name = "timetableId", required = true)
             ],
-            responses = [ApiResponse(responseCode = "200", content = [Content(schema = Schema(implementation = SharedTimetableBriefDto::class))])]
+            responses = [ApiResponse(responseCode = "200", content = [Content(schema = Schema(implementation = SharedTimetableDetailDto::class))])]
         ),
     ),
     RouterOperation(
@@ -48,25 +49,25 @@ import org.springframework.web.bind.annotation.RequestMethod
         ),
     ),
     RouterOperation(
-        path = "/v1/shared-tables/{id}", method = [RequestMethod.GET], produces = [MediaType.APPLICATION_JSON_VALUE],
+        path = "/v1/shared-tables/{id}", method = [RequestMethod.PUT], produces = [MediaType.APPLICATION_JSON_VALUE],
         operation = Operation(
             operationId = "updateSharedTimetables",
             parameters = [
                 Parameter(`in` = ParameterIn.HEADER, name = "userId", required = true),
                 Parameter(`in` = ParameterIn.PATH, name = "timetableId", required = true)
             ],
-            responses = [ApiResponse(responseCode = "200", content = [Content(schema = Schema(implementation = SharedTimetableModifyRequest::class))])]
+            requestBody = RequestBody(content = [Content(schema = Schema(implementation = SharedTimetableModifyRequest::class))]),
+            responses = [ApiResponse(responseCode = "200", content = [Content(schema = Schema())])]
         ),
     ),
     RouterOperation(
-        path = "/v1/shared-tables/", method = [RequestMethod.DELETE], produces = [MediaType.APPLICATION_JSON_VALUE],
+        path = "/v1/shared-tables/{id}", method = [RequestMethod.DELETE], produces = [MediaType.APPLICATION_JSON_VALUE],
         operation = Operation(
             operationId = "deleteSharedTimetable",
             parameters = [
                 Parameter(`in` = ParameterIn.HEADER, name = "userId", required = true),
                 Parameter(`in` = ParameterIn.PATH, name = "timetableId", required = true)
             ],
-            requestBody = RequestBody(content = [Content(schema = Schema(implementation = BookmarkLectureModifyRequest::class))]),
             responses = [ApiResponse(responseCode = "200", content = [Content(schema = Schema())])]
         ),
     ),
