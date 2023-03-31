62ab8477e63dfb4d39b95db2de91c8a30478c4a4
강지혁 <wlgur7238@snu.ac.kr>
2023-03-31 16:24:45 +0900

    route 설정
    @Bean, @XXXDocs 말고 @XXXApi 어떤가요?

--- api/src/main/kotlin/router/MainRouter.kt
+++ api/src/main/kotlin/router/MainRouter.kt
@@ -1,13 +1,7 @@
 package com.wafflestudio.snu4t.router
 
-import com.wafflestudio.snu4t.handler.AuthHandler
-import com.wafflestudio.snu4t.handler.BookmarkHandler
-import com.wafflestudio.snu4t.handler.SharedTimetableHandler
-import com.wafflestudio.snu4t.handler.TimetableHandler
-import com.wafflestudio.snu4t.router.docs.AuthDocs
-import com.wafflestudio.snu4t.router.docs.BookmarkDocs
-import com.wafflestudio.snu4t.router.docs.SharedTimetableDocs
-import com.wafflestudio.snu4t.router.docs.TableDocs
+import com.wafflestudio.snu4t.handler.*
+import com.wafflestudio.snu4t.router.docs.*
 import org.springframework.context.annotation.Bean
 import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.server.CoRouterFunctionDsl
@@ -21,6 +15,7 @@ class MainRouter(
     private val bookmarkHandler: BookmarkHandler,
     private val authHandler: AuthHandler,
     private val sharedTimetableHandler: SharedTimetableHandler,
+    private val notificationHandler: NotificationHandler,
 ) {
     @Bean
     fun ping() = coRouter {
@@ -62,6 +57,14 @@ class MainRouter(
             POST("", sharedTimetableHandler::addSharedTimetable)
             PUT("/{id}", sharedTimetableHandler::updateSharedTimetable)
             DELETE("/{id}", sharedTimetableHandler::deleteSharedTimetable)
+        }
+    }
+
+    @NotificationApi
+    fun notificationRoute() = v1CoRouter {
+        "/notification".nest {
+            GET("", notificationHandler::getNotification)
+            GET("/count", notificationHandler::getUnreadCounts)
         }
     }
 
