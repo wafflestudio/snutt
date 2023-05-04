9021844e842630cfb0e709609c99a7993e437ba3
davin111 <bdv111@snu.ac.kr>
2023-05-04 15:54:50 +0900

    webapplicationtype none

--- batch/src/main/kotlin/Snu4tBatchApplication.kt
+++ batch/src/main/kotlin/Snu4tBatchApplication.kt
@@ -1,17 +1,14 @@
 package com.wafflestudio.snu4t
 
 import org.springframework.boot.SpringApplication
+import org.springframework.boot.WebApplicationType
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
-import kotlin.system.exitProcess
 
 @SpringBootApplication
 class Snu4tBatchApplication
 
 fun main(args: Array<String>) {
-    runCatching {
-        val applicationContext = runApplication<Snu4tBatchApplication>(*args)
-        Thread.sleep(30000)
-        exitProcess(SpringApplication.exit(applicationContext))
-    }.onFailure { exception -> exception.printStackTrace() }
+    val application = SpringApplication(Snu4tBatchApplication::class.java)
+    application.webApplicationType = WebApplicationType.NONE
+    application.run(*args)
 }
