adce6b52635614fef4fde186c4d66da26717fcfb
davin111 <bdv111@snu.ac.kr>
2023-05-05 18:19:22 +0900

    login_local

--- core/src/main/kotlin/users/service/UserService.kt
+++ core/src/main/kotlin/users/service/UserService.kt
@@ -4,17 +4,22 @@ import com.wafflestudio.snu4t.common.exception.DuplicateLocalIdException
 import com.wafflestudio.snu4t.common.exception.InvalidEmailException
 import com.wafflestudio.snu4t.common.exception.InvalidLocalIdException
 import com.wafflestudio.snu4t.common.exception.InvalidPasswordException
+import com.wafflestudio.snu4t.common.exception.WrongLocalIdException
+import com.wafflestudio.snu4t.common.exception.WrongPasswordException
 import com.wafflestudio.snu4t.common.exception.WrongUserTokenException
 import com.wafflestudio.snu4t.users.data.User
+import com.wafflestudio.snu4t.users.dto.LocalLoginRequest
 import com.wafflestudio.snu4t.users.dto.LocalRegisterRequest
-import com.wafflestudio.snu4t.users.dto.RegisterResponse
+import com.wafflestudio.snu4t.users.dto.LoginResponse
 import com.wafflestudio.snu4t.users.repository.UserRepository
 import org.springframework.stereotype.Service
 
 interface UserService {
     suspend fun getUserByCredentialHash(credentialHash: String): User
 
-    suspend fun registerLocal(localRegisterRequest: LocalRegisterRequest): RegisterResponse
+    suspend fun registerLocal(localRegisterRequest: LocalRegisterRequest): LoginResponse
+
+    suspend fun loginLocal(localRegisterRequest: LocalLoginRequest): LoginResponse
 
     suspend fun update(user: User): User
 }
@@ -27,7 +32,7 @@ class UserServiceImpl(
     override suspend fun getUserByCredentialHash(credentialHash: String): User =
         userRepository.findByCredentialHashAndActive(credentialHash, true) ?: throw WrongUserTokenException
 
-    override suspend fun registerLocal(localRegisterRequest: LocalRegisterRequest): RegisterResponse {
+    override suspend fun registerLocal(localRegisterRequest: LocalRegisterRequest): LoginResponse {
         val localId = localRegisterRequest.id
         val password = localRegisterRequest.password
         val email = localRegisterRequest.email
@@ -51,9 +56,23 @@ class UserServiceImpl(
             )
         )
 
-        return RegisterResponse(
+        return LoginResponse(
             userId = user.id!!,
             token = credentialHash,
+        )
+    }
+
+    override suspend fun loginLocal(localRegisterRequest: LocalLoginRequest): LoginResponse {
+        val localId = localRegisterRequest.id
+        val password = localRegisterRequest.password
+
+        val user = userRepository.findByCredentialLocalIdAndActiveTrue(localId) ?: throw WrongLocalIdException
+
+        if (!authService.isMatchedPassword(user, password)) throw WrongPasswordException
+
+        return LoginResponse(
+            userId = user.id!!,
+            token = user.credentialHash,
         )
     }
 
