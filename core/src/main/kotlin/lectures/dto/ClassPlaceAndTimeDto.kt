23b71d09d61c68e1ecc97ceeb1147632e9f495b5
Hank-Choi <zlzlqlzl1@naver.com>
2023-05-12 20:15:46 +0900

    json 이름 변경
    json 필드 이름 변경
    
    json 필드 이름 변경

--- core/src/main/kotlin/lectures/dto/ClassPlaceAndTimeDto.kt
+++ core/src/main/kotlin/lectures/dto/ClassPlaceAndTimeDto.kt
@@ -8,13 +8,14 @@ import com.wafflestudio.snu4t.lectures.data.ClassPlaceAndTime
 import com.wafflestudio.snu4t.lectures.utils.endPeriod
 import com.wafflestudio.snu4t.lectures.utils.startPeriod
 
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class ClassPlaceAndTimeDto(
     val day: DayOfWeek,
     val place: String?,
     val startMinute: Int,
     val endMinute: Int,
+    @JsonProperty("start_time")
     val startTime: String,
+    @JsonProperty("end_time")
     val endTime: String,
     @JsonProperty("len")
     val periodLength: Double,
--- core/src/main/kotlin/lectures/dto/LectureDto.kt
+++ core/src/main/kotlin/lectures/dto/LectureDto.kt
@@ -7,23 +7,31 @@ import com.wafflestudio.snu4t.lectures.data.Lecture
 data class LectureDto(
     @JsonProperty("_id")
     val id: String? = null,
+    @JsonProperty("academic_year")
     val academicYear: String?,
     val category: String?,
+    @JsonProperty("class_time")
     val periodText: String?,
+    @JsonProperty("real_class_time")
     val classTimeText: String?,
+    @JsonProperty("class_time_json")
     val classPlaceAndTimes: List<ClassPlaceAndTimeDto>,
+    @JsonProperty("class_time_mask")
     val classTimeMask: List<Int>,
     val classification: String?,
     val credit: Long,
     val department: String?,
     val instructor: String?,
+    @JsonProperty("lecture_number")
     val lectureNumber: String,
     val quota: Int?,
     val freshmanQuota: Int? = null,
     val remark: String?,
     val semester: Semester,
     val year: Int,
+    @JsonProperty("course_number")
     val courseNumber: String,
+    @JsonProperty("course_title")
     val courseTitle: String,
     val registrationCount: Int,
 )
