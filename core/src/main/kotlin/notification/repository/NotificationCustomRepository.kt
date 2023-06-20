02c8b0280a2a29220fcbafd6aba83463d670df2a
Hank-Choi <zlzlqlzl1@naver.com>
2023-06-21 02:47:36 +0900

    깔끔하게 수정

--- core/src/main/kotlin/notification/repository/NotificationCustomRepository.kt
+++ core/src/main/kotlin/notification/repository/NotificationCustomRepository.kt
@@ -1,9 +1,10 @@
 package com.wafflestudio.snu4t.notification.repository
 
+import com.wafflestudio.snu4t.common.desc
+import com.wafflestudio.snu4t.common.gt
 import com.wafflestudio.snu4t.notification.data.Notification
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.reactive.asFlow
-import org.springframework.data.domain.Sort
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate
 import org.springframework.data.mongodb.core.find
 import org.springframework.data.mongodb.core.query.Query
@@ -19,8 +20,7 @@ class NotificationRepositoryImpl(private val reactiveMongoTemplate: ReactiveMong
     override fun findNotifications(userId: String, createdAt: LocalDateTime, offset: Long, limit: Int) =
         reactiveMongoTemplate.find<Notification>(
             Query.query(
-                Notification::userId.inValues(listOf(null, userId))
-                    .and(Notification::createdAt).gt(createdAt)
-            ).with(Sort.sort(Notification::createdAt.javaClass).descending()).skip(offset).limit(limit)
+                Notification::userId inValues listOf(null, userId) and Notification::createdAt gt createdAt
+            ).with(Notification::createdAt.desc()).skip(offset).limit(limit)
         ).asFlow()
 }
