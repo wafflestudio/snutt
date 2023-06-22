110c7917494772c941cab2c145002146308b64a4
Hankyeol Choi <zlzlqlzl1@naver.com>
2023-06-22 17:50:39 +0900

    알림 조회 조건 오류 수정 (#94)

--- core/src/main/kotlin/notification/repository/NotificationRepository.kt
+++ core/src/main/kotlin/notification/repository/NotificationRepository.kt
@@ -1,28 +1,13 @@
 package com.wafflestudio.snu4t.notification.repository
 
 import com.wafflestudio.snu4t.notification.data.Notification
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.toList
-import org.springframework.data.domain.Pageable
 import org.springframework.data.repository.kotlin.CoroutineCrudRepository
 import org.springframework.stereotype.Repository
 import java.time.LocalDateTime
 
 @Repository
-interface NotificationRepository : CoroutineCrudRepository<Notification, String> {
-    fun findAllByUserIdInAndCreatedAtGreaterThanOrderByCreatedAtDesc(userIds: List<String?>, createdAt: LocalDateTime, pageable: Pageable): Flow<Notification>
+interface NotificationRepository : CoroutineCrudRepository<Notification, String>, NotificationCustomRepository {
     suspend fun countByUserIdInAndCreatedAtAfter(userIds: List<String?>, createdAt: LocalDateTime): Long
-}
-
-// 전체 공지도 포함해야 함 (전체공지는 userId = null)
-// 따라서 in 절 쿼리를 사용하였음
-
-suspend fun NotificationRepository.findNotifications(
-    userId: String,
-    createdAt: LocalDateTime,
-    pageable: Pageable
-): List<Notification> {
-    return findAllByUserIdInAndCreatedAtGreaterThanOrderByCreatedAtDesc(listOf(userId, null), createdAt, pageable).toList()
 }
 
 suspend fun NotificationRepository.countUnreadNotifications(userId: String, createdAt: LocalDateTime): Long {
