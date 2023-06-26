0789c3ad94f855a2d4ddb2376a92757a6461fe05
davin111 <bdv111@snu.ac.kr>
2023-06-26 20:19:21 +0900

    reflect reviews

--- core/src/main/kotlin/notification/service/NotificationService.kt
+++ core/src/main/kotlin/notification/service/NotificationService.kt
@@ -3,14 +3,16 @@ package com.wafflestudio.snu4t.notification.service
 import com.wafflestudio.snu4t.common.push.PushClient
 import com.wafflestudio.snu4t.common.push.dto.PushMessage
 import com.wafflestudio.snu4t.common.push.dto.PushTargetMessage
+import com.wafflestudio.snu4t.notification.data.Notification
 import com.wafflestudio.snu4t.notification.dto.NotificationQuery
-import com.wafflestudio.snu4t.notification.dto.NotificationResponse
 import com.wafflestudio.snu4t.notification.repository.NotificationRepository
 import com.wafflestudio.snu4t.notification.repository.countUnreadNotifications
 import com.wafflestudio.snu4t.users.data.User
 import com.wafflestudio.snu4t.users.service.UserService
+import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.toList
+import kotlinx.coroutines.launch
 import org.springframework.stereotype.Service
 import java.time.LocalDateTime
 
@@ -21,7 +23,7 @@ class NotificationService internal constructor(
     private val pushClient: PushClient,
     private val notificationRepository: NotificationRepository,
 ) {
-    suspend fun getNotifications(query: NotificationQuery): List<NotificationResponse> {
+    suspend fun getNotifications(query: NotificationQuery): List<Notification> {
         val user = query.user
         val notifications = notificationRepository.findNotifications(
             userId = user.id!!,
@@ -34,38 +36,42 @@ class NotificationService internal constructor(
             userService.update(user.apply { notificationCheckedAt = LocalDateTime.now() })
         }
 
-        return notifications.map { NotificationResponse.from(it) }
+        return notifications
     }
 
     suspend fun getUnreadCount(user: User): Long {
         return notificationRepository.countUnreadNotifications(user.id!!, user.notificationCheckedAt)
     }
 
-    suspend fun sendPush(userId: String, pushMessage: PushMessage) {
-        saveNotification(userId, pushMessage)
+    suspend fun sendPush(userId: String, pushMessage: PushMessage) = coroutineScope {
+        launch { saveNotification(userId, pushMessage) }
 
-        val userDevices = deviceService.getUserDevices(userId).ifEmpty { return }
+        launch {
+            val userDevices = deviceService.getUserDevices(userId).ifEmpty { return@launch }
 
-        val pushTargetMessages = userDevices.map { PushTargetMessage(it.fcmRegistrationId, pushMessage) }
-        pushClient.sendMessages(pushTargetMessages)
-    }
-
-    suspend fun sendPushes(userIds: List<String>, pushMessage: PushMessage) {
-        saveNotifications(userIds, pushMessage)
-
-        val userIdToDevices = deviceService.getUsersDevices(userIds).ifEmpty { return }
-
-        val pushTargetMessages = userIdToDevices.values.flatMap { userDevices ->
-            userDevices.map { PushTargetMessage(it.fcmRegistrationId, pushMessage) }
+            val pushTargetMessages = userDevices.map { PushTargetMessage(it.fcmRegistrationId, pushMessage) }
+            pushClient.sendMessages(pushTargetMessages)
         }
-
-        pushClient.sendMessages(pushTargetMessages)
     }
 
-    suspend fun sendGlobalPush(pushMessage: PushMessage) {
-        saveNotification(userId = null, pushMessage)
+    suspend fun sendPushes(userIds: List<String>, pushMessage: PushMessage) = coroutineScope {
+        launch { saveNotifications(userIds, pushMessage) }
 
-        pushClient.sendGlobalMessage(pushMessage)
+        launch {
+            val userIdToDevices = deviceService.getUsersDevices(userIds).ifEmpty { return@launch }
+
+            val pushTargetMessages = userIdToDevices.values.flatMap { userDevices ->
+                userDevices.map { PushTargetMessage(it.fcmRegistrationId, pushMessage) }
+            }
+
+            pushClient.sendMessages(pushTargetMessages)
+        }
+    }
+
+    suspend fun sendGlobalPush(pushMessage: PushMessage) = coroutineScope {
+        launch { saveNotification(userId = null, pushMessage) }
+
+        launch { pushClient.sendGlobalMessage(pushMessage) }
     }
 
     private suspend fun saveNotification(userId: String?, pushMessage: PushMessage) {
