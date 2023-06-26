2f7d0ec6963fbfba4dc1ed356718a918935a0554
Hankyeol Choi <zlzlqlzl1@naver.com>
2023-06-26 21:12:18 +0900

    classTimeMask response에서 빈 값 주도록 롤백 (#96)

--- core/src/main/kotlin/lectures/dto/BookmarkLectureDto.kt
+++ core/src/main/kotlin/lectures/dto/BookmarkLectureDto.kt
@@ -1,14 +1,12 @@
 package com.wafflestudio.snu4t.lectures.dto
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.databind.PropertyNamingStrategies
-import com.fasterxml.jackson.databind.annotation.JsonNaming
 import com.wafflestudio.snu4t.lectures.data.BookmarkLecture
 
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class BookmarkLectureDto(
     @JsonProperty("_id")
     var id: String? = null,
+    @JsonProperty("academic_year")
     var academicYear: String?,
     var category: String?,
     @JsonProperty("class_time_json")
@@ -17,12 +15,19 @@ data class BookmarkLectureDto(
     var credit: Long,
     var department: String?,
     var instructor: String?,
+    @JsonProperty("lecture_number")
     var lectureNumber: String,
     var quota: Int?,
     var freshmanQuota: Int?,
     var remark: String?,
+    @JsonProperty("course_number")
     var courseNumber: String,
+    @JsonProperty("course_title")
     var courseTitle: String,
+
+    // FIXME: 안드로이드 구버전 대응용 필드 1년 후 2024년에 삭제 (2023/06/26)
+    @JsonProperty("class_time_mask")
+    val classTimeMask: List<Int> = emptyList(),
 )
 
 fun BookmarkLectureDto(lecture: BookmarkLecture): BookmarkLectureDto = BookmarkLectureDto(
