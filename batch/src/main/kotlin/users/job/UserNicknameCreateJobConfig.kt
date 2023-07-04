31a82f4a0f0c5a86422dc10ef796a547566e6f08
강지혁 <wlgur7238@snu.ac.kr>
2023-07-04 23:29:09 +0900

    batch size 삭제, retry 로직 수정, concurrency 100으로 늘림
    Flow.retry 쓰면 안됨
    + 듑 에러일때만 로그 찍도록 수정
    + elapsed time and update count 로그

--- batch/src/main/kotlin/users/job/UserNicknameCreateJobConfig.kt
+++ batch/src/main/kotlin/users/job/UserNicknameCreateJobConfig.kt
@@ -1,12 +1,12 @@
 package com.wafflestudio.snu4t.users.job
 
-import com.wafflestudio.snu4t.common.isEqualTo
+import com.wafflestudio.snu4t.common.extension.isEqualTo
 import com.wafflestudio.snu4t.users.data.User
 import com.wafflestudio.snu4t.users.service.UserNicknameGenerateService
-import kotlinx.coroutines.flow.count
-import kotlinx.coroutines.flow.map
-import kotlinx.coroutines.flow.retryWhen
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.reactive.asFlow
+import kotlinx.coroutines.reactive.awaitSingle
 import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
 import org.springframework.batch.core.Job
@@ -22,6 +22,9 @@ import org.springframework.data.mongodb.core.ReactiveMongoTemplate
 import org.springframework.data.mongodb.core.find
 import org.springframework.data.mongodb.core.query.Query
 import org.springframework.transaction.PlatformTransactionManager
+import java.util.concurrent.atomic.AtomicInteger
+import kotlin.time.ExperimentalTime
+import kotlin.time.measureTimedValue
 
 @Configuration
 class UserNicknameCreateJobConfig(
@@ -33,7 +36,6 @@ class UserNicknameCreateJobConfig(
     private companion object {
         const val JOB_NAME = "userNicknameCreateJob"
         const val STEP_NAME = "userNicknameCreateStep"
-        private const val BATCH_SIZE = 2000
     }
 
     @Bean
@@ -44,36 +46,53 @@ class UserNicknameCreateJobConfig(
     }
 
     @Bean
+    @OptIn(ExperimentalTime::class)
     fun userNicknameCreateStep(
         jobRepository: JobRepository,
         transactionManager: PlatformTransactionManager,
     ): Step = StepBuilder(STEP_NAME, jobRepository).tasklet(
         { _, _ ->
-            runBlocking {
-                var updateCount: Int
-                do { updateCount = updateUserNickname() }
-                while (updateCount > 0)
-            }
+            val (updateCount, elapsedTime) = measureTimedValue { updateUserNickname() }
+            log.info("닉네임 생성 작업이 완료되었습니다. 총 ${updateCount}명 유저의 닉네임을 생성했습니다. (소요시간: ${elapsedTime.inWholeSeconds}초)")
             RepeatStatus.FINISHED
         },
         transactionManager
     ).build()
 
-    private suspend fun updateUserNickname(): Int {
-        return reactiveMongoTemplate.find<User>(Query.query(User::nickname isEqualTo null).limit(BATCH_SIZE))
-            .asFlow()
-            .map { tryToUpdateNickname(it) }
-            .retryWhen { cause, attempt ->
-                log.info("닉네임 충돌 발생! ${attempt}번째 재시도 중 ($cause)")
-                cause is DuplicateKeyException && attempt < 100
+    private fun updateUserNickname(): Int = runBlocking {
+        val updateCount = AtomicInteger()
+        val channel = Channel<User>(capacity = 100)
+
+        repeat(100) {
+            launch {
+                for (user in channel) {
+                    tryToUpdateNickname(user)
+                    updateCount.incrementAndGet()
+                }
             }
-            .count()
+        }
+
+        reactiveMongoTemplate.find<User>(Query.query(User::nickname isEqualTo null))
+            .asFlow()
+            .collect { channel.send(it) }
+
+        channel.close()
+        updateCount.get()
     }
 
     private suspend fun tryToUpdateNickname(user: User) {
         val nickname = userNicknameGenerateService.generateRandomNickname()
         val updated = user.copy(nickname = nickname)
-        reactiveMongoTemplate.save(updated).block()
-        log.info("유저(id: #${user.id}, email:#${user.email})의 닉네임을 ${updated.nickname}로 변경했습니다.")
+        try {
+            reactiveMongoTemplate.save(updated).awaitSingle()
+            log.info("유저(id: #${user.id}, email:#${user.email})의 닉네임을 ${updated.nickname}로 변경했습니다.")
+        } catch (e: DuplicateKeyException) {
+            log.info("유저(id: #${user.id}, email:#${user.email})의 닉네임 ${user.nickname}이 중복되어 재시도합니다.")
+            val unique = user.copy(nickname = userNicknameGenerateService.generateUniqueRandomNickname())
+            reactiveMongoTemplate.save(unique).awaitSingle()
+            log.info("유저(id: #${user.id}, email:#${user.email})의 닉네임을 ${unique.nickname}로 변경했습니다.")
+        } catch (e: Exception) {
+            log.error("유저(id: #${user.id}, email:#${user.email})의 닉네임 ${user.nickname} 변경에 실패했습니다.")
+        }
     }
 }
