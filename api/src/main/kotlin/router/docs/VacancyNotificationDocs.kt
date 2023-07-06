bd1fd5f8afd88cb01ab6c481f2c5039d6f9d5ebd
Hank-Choi <zlzlqlzl1@naver.com>
2023-07-06 18:38:37 +0900

    docs 업데이트

--- api/src/main/kotlin/router/docs/VacancyNotificationDocs.kt
+++ api/src/main/kotlin/router/docs/VacancyNotificationDocs.kt
@@ -1,9 +1,10 @@
 package com.wafflestudio.snu4t.router.docs
 
-import com.wafflestudio.snu4t.vacancynotification.dto.VacancyNotificationDto
+import com.wafflestudio.snu4t.lectures.data.Lecture
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.enums.ParameterIn
+import io.swagger.v3.oas.annotations.media.ArraySchema
 import io.swagger.v3.oas.annotations.media.Content
 import io.swagger.v3.oas.annotations.media.Schema
 import io.swagger.v3.oas.annotations.responses.ApiResponse
@@ -14,21 +15,28 @@ import org.springframework.web.bind.annotation.RequestMethod
 
 @RouterOperations(
     RouterOperation(
+        path = "/v1/vacancy-notifications/lectures", method = [RequestMethod.GET], produces = [MediaType.APPLICATION_JSON_VALUE],
+        operation = Operation(
+            operationId = "getVacancyNotificationLectures",
+            responses = [ApiResponse(responseCode = "200", content = [Content(array = ArraySchema(schema =  Schema(implementation = Lecture::class)))])]
+        ),
+    ),
+    RouterOperation(
         path = "/v1/vacancy-notifications/lectures/{lectureId}", method = [RequestMethod.POST], produces = [MediaType.APPLICATION_JSON_VALUE],
         operation = Operation(
             operationId = "addVacancyNotification",
             parameters = [
                 Parameter(`in` = ParameterIn.PATH, name = "lectureId", required = true),
             ],
-            responses = [ApiResponse(responseCode = "200", content = [Content(schema = Schema(implementation = VacancyNotificationDto::class))])]
+            responses = [ApiResponse(responseCode = "200", content = [Content(schema = Schema())])]
         ),
     ),
     RouterOperation(
-        path = "/v1/vacancy-notifications/{id}", method = [RequestMethod.DELETE], produces = [MediaType.APPLICATION_JSON_VALUE],
+        path = "/v1/vacancy-notifications/lectures/{lectureId}", method = [RequestMethod.DELETE], produces = [MediaType.APPLICATION_JSON_VALUE],
         operation = Operation(
             operationId = "deleteVacancyNotification",
             parameters = [
-                Parameter(`in` = ParameterIn.PATH, name = "id", required = true),
+                Parameter(`in` = ParameterIn.PATH, name = "lectureId", required = true),
             ],
             responses = [ApiResponse(responseCode = "200", content = [Content(schema = Schema())])]
         ),
