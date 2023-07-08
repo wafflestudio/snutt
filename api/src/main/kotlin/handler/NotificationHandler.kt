9bfe205af98b7d0d02c73572e859b1599347cf36
Davin Byeon <bdv111@snu.ac.kr>
2023-07-08 19:37:42 +0900

    Fix noti count resp (#111)

--- api/src/main/kotlin/handler/NotificationHandler.kt
+++ api/src/main/kotlin/handler/NotificationHandler.kt
@@ -1,6 +1,7 @@
 package com.wafflestudio.snu4t.handler
 
 import com.wafflestudio.snu4t.middleware.SnuttRestApiDefaultMiddleware
+import com.wafflestudio.snu4t.notification.dto.NotificationCountResponse
 import com.wafflestudio.snu4t.notification.dto.NotificationQuery
 import com.wafflestudio.snu4t.notification.dto.NotificationResponse
 import com.wafflestudio.snu4t.notification.service.NotificationService
@@ -25,6 +26,7 @@ class NotificationHandler(
 
     suspend fun getUnreadCounts(req: ServerRequest) = handle(req) {
         val user = req.getContext().user!!
-        notificationService.getUnreadCount(user)
+        val unreadCount = notificationService.getUnreadCount(user)
+        NotificationCountResponse(unreadCount)
     }
 }
---
+++ core/src/main/kotlin/notification/dto/NotificationCountResponse.kt
@@ -0,0 +1,5 @@
+package com.wafflestudio.snu4t.notification.dto
+
+data class NotificationCountResponse(
+    val count: Long,
+)
