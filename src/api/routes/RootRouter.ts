dd056cc67a171d7ebc14a5cefd0a8c5f147c21e5
davin111 <bdv111@snu.ac.kr>
2023-08-02 22:05:16 +0900

    health check db connection

--- src/api/routes/RootRouter.ts
+++ src/api/routes/RootRouter.ts
@@ -2,10 +2,25 @@ import ExpressPromiseRouter from 'express-promise-router';
 import MonitorRouter = require('./MonitorRouter');
 import StaticPageRouter = require('./StaticPageRouter');
 import ApiRouter = require('./ApiRouter');
+import mongoose = require('mongoose');
 
 let router = ExpressPromiseRouter();
 
-router.get('/health-check', (req, res) => res.sendStatus(200));
+router.get('/health-check', (req, res) => {
+  if (mongoose.connection.readyState !== 1) {
+    res.status(500).json({ message: 'MongoDB connection failed' });
+    return;
+  }
+
+  mongoose.connection.db.admin().ping((err, result) => {
+    if (err) {
+      res.status(500).json({ message: 'MongoDB connection failed' });
+      return;
+    }
+    res.status(200).json({ message: 'ok' });
+  });
+});
+
 router.use('/monitor', MonitorRouter);
 
 router.use(function(req, res) {
