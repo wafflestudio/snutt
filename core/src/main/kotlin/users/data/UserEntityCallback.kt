1888a845e8da66ace3d80052e2ec0a4854a2a0eb
강지혁 <wlgur7238@snu.ac.kr>
2023-09-09 03:21:30 +0900

    Release (#172)

---
+++ core/src/main/kotlin/users/data/UserEntityCallback.kt
@@ -0,0 +1,42 @@
+package com.wafflestudio.snu4t.users.data
+
+import com.wafflestudio.snu4t.users.repository.UserRepository
+import com.wafflestudio.snu4t.users.service.UserNicknameService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.reactor.mono
+import org.bson.Document
+import org.reactivestreams.Publisher
+import org.slf4j.LoggerFactory
+import org.springframework.context.annotation.Lazy
+import org.springframework.data.mongodb.core.mapping.event.ReactiveAfterConvertCallback
+import org.springframework.stereotype.Component
+import reactor.core.publisher.Mono
+
+@Component
+class UserEntityCallback @Lazy constructor(
+    private val userRepository: UserRepository,
+    private val userNicknameService: UserNicknameService,
+) : ReactiveAfterConvertCallback<User> {
+
+    private val log = LoggerFactory.getLogger(javaClass)
+
+    override fun onAfterConvert(entity: User, document: Document, collection: String): Publisher<User> {
+        if (entity.nickname != null) {
+            return Mono.just(entity)
+        }
+
+        log.info("empty nickname found. user: $entity")
+        return backFillNickname(entity)
+    }
+
+    private fun backFillNickname(user: User): Mono<User> = mono(Dispatchers.Unconfined) {
+        try {
+            val result = userRepository.save(user.copy(nickname = userNicknameService.generateUniqueRandomNickname()))
+            log.info("[BACKFILL] updated nickname of USER: $result")
+            result
+        } catch (e: Exception) {
+            log.error("[BACKFILL] failed to update nickname of USER: $user", e)
+            throw e
+        }
+    }
+}
