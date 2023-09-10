1a8daa259a0542073ee93b8421a57d8028221778
강지혁 <wlgur7238@snu.ac.kr>
2023-09-11 02:06:52 +0900

    연도별로 끊어서 작업 + lint

--- batch/src/main/kotlin/timetables/job/AutoPrimaryJobConfig.kt
+++ batch/src/main/kotlin/timetables/job/AutoPrimaryJobConfig.kt
@@ -11,26 +11,25 @@ import io.github.resilience4j.ratelimiter.RateLimiterConfig
 import kotlinx.coroutines.flow.*
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.reactive.asFlow
-import kotlinx.coroutines.reactor.mono
 import kotlinx.coroutines.runBlocking
 import org.slf4j.LoggerFactory
 import org.springframework.batch.core.Job
 import org.springframework.batch.core.Step
+import org.springframework.batch.core.configuration.annotation.JobScope
 import org.springframework.batch.core.job.builder.JobBuilder
 import org.springframework.batch.core.repository.JobRepository
 import org.springframework.batch.core.step.builder.StepBuilder
 import org.springframework.batch.repeat.RepeatStatus
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.mongodb.core.BulkOperations
 import org.springframework.data.mongodb.core.ReactiveMongoTemplate
-import org.springframework.data.mongodb.core.ReactiveUpdateOperation.UpdateWithQuery
 import org.springframework.data.mongodb.core.aggregation.Aggregation
 import org.springframework.data.mongodb.core.count
 import org.springframework.data.mongodb.core.query.Criteria
 import org.springframework.data.mongodb.core.query.Query
 import org.springframework.data.mongodb.core.query.Update
-import org.springframework.data.mongodb.core.query.UpdateDefinition
 import org.springframework.transaction.PlatformTransactionManager
 import java.time.Duration
 import java.util.concurrent.ConcurrentHashMap
@@ -53,12 +52,14 @@ class AutoPrimaryJobConfig(
     }
 
     @Bean
+    @JobScope
     fun primaryTimetableAutoSetStep(
         jobRepository: JobRepository,
         transactionManager: PlatformTransactionManager,
+        @Value("#{jobParameters[year]}") year: Int
     ): Step = StepBuilder(STEP_NAME, jobRepository).tasklet(
         { _, _ ->
-            autoSetPrimaryTimetable()
+            autoSetPrimaryTimetable(year)
             RepeatStatus.FINISHED
         },
         transactionManager
@@ -66,11 +67,11 @@ class AutoPrimaryJobConfig(
 
     data class AggResult(val id: Key)
     data class Key(val user_id: String, val semester: Semester, val year: Int)
-    private fun autoSetPrimaryTimetable() = runBlocking {
+    private fun autoSetPrimaryTimetable(year: Int) = runBlocking {
         val counter = AtomicInteger()
         val timetablesCount = reactiveMongoTemplate.count(
-                Query.query(Criteria.where("_id").ne(null)),
-                Timetable::class.java
+            Query.query(Criteria.where("_id").ne(null).and("year").`is`(year)),
+            Timetable::class.java
         ).block() ?: 0L
 
         val rateLimiter = RateLimiter.of(
@@ -82,7 +83,11 @@ class AutoPrimaryJobConfig(
             }
         )
 
-        val agg = Aggregation.newAggregation(Key::class.java, Aggregation.group("user_id", "semester", "year"))
+        val agg = Aggregation.newAggregation(
+            Key::class.java,
+            Aggregation.match(Criteria.where("year").`is`(year)),
+            Aggregation.group("user_id", "semester", "year")
+        )
         val buffer = ConcurrentHashMap.newKeySet<String>()
         reactiveMongoTemplate.aggregate(agg, "timetables", AggResult::class.java)
             .asFlow()
@@ -99,18 +104,18 @@ class AutoPrimaryJobConfig(
                 val ids = HashSet(buffer).also { buffer.clear() }
                 launch {
                     reactiveMongoTemplate.bulkOps(
-                            BulkOperations.BulkMode.ORDERED, "timetables"
+                        BulkOperations.BulkMode.ORDERED, "timetables"
                     ).updateMulti(
-                            Query.query(Criteria.where("_id").`in`(ids)),
-                            Update.update("isPrimary", true)
+                        Query.query(Criteria.where("_id").`in`(ids)),
+                        Update.update("isPrimary", true)
                     ).execute().block()
                 }.join()
-                log.info("updated ${buffer.size} docs")
+                log.info("updated ${ids.size} docs")
             }
 
         if (buffer.isNotEmpty()) {
             reactiveMongoTemplate.bulkOps(
-                    BulkOperations.BulkMode.ORDERED, "timetables"
+                BulkOperations.BulkMode.ORDERED, "timetables"
             ).updateMulti(
                 Query.query(Criteria.where("_id").`in`(buffer)),
                 Update.update("is_primary", true)
@@ -122,12 +127,12 @@ class AutoPrimaryJobConfig(
         val (userId, semester, year) = key
         val timetables = timetableRepository.findAllByUserIdAndYearAndSemester(userId, year, semester).toList()
         if (timetables.any { it.isPrimary == true }) {
-            log.info("[${counter.addAndGet(timetables.size)}/${timetablesCount}] SKIPPED $key | primary table alrady exists")
+            log.info("[${counter.addAndGet(timetables.size)}/$timetablesCount] SKIPPED $key | primary table alrady exists")
             return null
         }
 
         val newPrimaryTimetable = timetables.maxBy { it.updatedAt }
-        log.info("[${counter.addAndGet(timetables.size)}/${timetablesCount}] UPDATING $key | primary table ID : ${newPrimaryTimetable.id}")
+        log.info("[${counter.addAndGet(timetables.size)}/$timetablesCount] UPDATING $key | primary table ID : ${newPrimaryTimetable.id}")
         return newPrimaryTimetable.id
     }
 
