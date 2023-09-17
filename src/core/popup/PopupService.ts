4512e6156984c36697cfe125d637e01cf33f8418
Davin Byeon <bdv111@snu.ac.kr>
2023-09-17 19:40:29 +0900

    Update PopupService.ts

--- src/core/popup/PopupService.ts
+++ src/core/popup/PopupService.ts
@@ -2,6 +2,12 @@ import User from '@app/core/user/model/User';
 
 export async function getPopups(user: User, osType?: string, osVersion?: string, appType?: string, appVersion?: string) {
   return {
-    content: [],
+    content: [
+       {
+         "key": "new-service-friend",
+         "image_url": "https://snutt-asset.s3.ap-northeast-2.amazonaws.com/popup-images/friendPopup.png",
+         "hidden_days": 1,
+       },
+     ],
   };
 }
