14f799d2cbabe2cead4c6e6cdbef39965ab139c6
Subeen Park <psb021414@gmail.com>
2024-01-10 09:31:46 +0900

    강의동 Mock 데이터 추가

--- core/src/main/kotlin/lectures/dto/ClassPlaceAndTimeDto.kt
+++ core/src/main/kotlin/lectures/dto/ClassPlaceAndTimeDto.kt
@@ -2,10 +2,14 @@ package com.wafflestudio.snu4t.lectures.dto
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.wafflestudio.snu4t.common.enum.DayOfWeek
+import com.wafflestudio.snu4t.lecturehalls.data.Campus
+import com.wafflestudio.snu4t.lecturehalls.data.GeoCoordinate
+import com.wafflestudio.snu4t.lecturehalls.data.LectureHall
 import com.wafflestudio.snu4t.lectures.data.ClassPlaceAndTime
 import com.wafflestudio.snu4t.lectures.utils.endPeriod
 import com.wafflestudio.snu4t.lectures.utils.minuteToString
 import com.wafflestudio.snu4t.lectures.utils.startPeriod
+import java.util.UUID
 
 data class ClassPlaceAndTimeDto(
     val day: DayOfWeek,
@@ -34,6 +38,7 @@ data class ClassPlaceAndTimeLegacyDto(
     val periodLength: Double,
     @JsonProperty("start")
     val startPeriod: Double,
+    val lectureHall: LectureHall?
 )
 
 fun ClassPlaceAndTimeLegacyDto(classPlaceAndTime: ClassPlaceAndTime): ClassPlaceAndTimeLegacyDto = ClassPlaceAndTimeLegacyDto(
@@ -45,4 +50,15 @@ fun ClassPlaceAndTimeLegacyDto(classPlaceAndTime: ClassPlaceAndTime): ClassPlace
     endTime = minuteToString(classPlaceAndTime.endMinute),
     startPeriod = classPlaceAndTime.startPeriod,
     periodLength = classPlaceAndTime.endPeriod - classPlaceAndTime.startPeriod,
+    lectureHall = MockLectureHall()
+)
+
+private fun MockLectureHall() = LectureHall(
+    id = UUID.randomUUID().toString(),
+    buildingNumber = "500",
+    buildingNameKor = "자연과학대학(500)",
+    buildingNameEng = "College of Natural Sciences(500)",
+    locationInDMS = GeoCoordinate(37.4592190840394, 126.94812006718699),
+    locationInDecimal = GeoCoordinate(488525.0, 1099948.0),
+    campus = Campus.GWANAK
 )
