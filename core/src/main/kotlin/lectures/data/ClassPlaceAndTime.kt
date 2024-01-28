ca9a413a57ece3d82152ffb363e4e16de4f9f985
Subeen Park <psb021414@gmail.com>
2024-01-28 22:25:13 +0900

    강의 업데이트 비교시 강의동 정보를 제외

--- core/src/main/kotlin/lectures/data/ClassPlaceAndTime.kt
+++ core/src/main/kotlin/lectures/data/ClassPlaceAndTime.kt
@@ -8,5 +8,19 @@ data class ClassPlaceAndTime(
     val place: String,
     val startMinute: Int,
     val endMinute: Int,
-    var lectureBuilding: LectureBuilding?
-)
+    var lectureBuilding: LectureBuilding? = null
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        return when (other) {
+            is ClassPlaceAndTime -> {
+                this.day == other.day &&
+                    this.place == other.place &&
+                    this.startMinute == other.startMinute &&
+                    this.endMinute == other.endMinute
+            }
+
+            else -> false
+        }
+    }
+}
