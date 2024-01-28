548b2e0aed30abb4ee261f6fe986207b3a3ab831
Subeen Park <psb021414@gmail.com>
2024-01-29 00:31:09 +0900

    강의실이 여러개인 경우, 세부 호수가 붙은 경우 대응

--- batch/src/main/kotlin/lecturebuildings/data/PlaceInfo.kt
+++ batch/src/main/kotlin/lecturebuildings/data/PlaceInfo.kt
@@ -1,19 +1,31 @@
 package com.wafflestudio.snu4t.lecturebuildings.data
 
 class PlaceInfo(
+    val rawString: String,
     val campus: Campus,
-    val buildingNumber: String
-)
-
-fun PlaceInfo(place: String): PlaceInfo? = place
-    .split("-").dropLast(1).joinToString("-")
-    .ifBlank { null }
-    ?.let {
-        var (buildingNumber, campus) = when (it.first()) {
-            '#' -> Pair(it.removePrefix("#"), Campus.YEONGEON)
-            '*' -> Pair(it.removePrefix("*"), Campus.PYEONGCHANG)
-            else -> Pair(it, Campus.GWANAK)
-        }
-
-        PlaceInfo(campus, buildingNumber.trimStart { it == '0' })
+    val buildingNumber: String,
+) {
+    companion object {
+        fun getValuesOf(places: String): List<PlaceInfo> = places.split("/").map { PlaceInfo(it) }.filterNotNull()
     }
+}
+
+fun PlaceInfo(place: String): PlaceInfo? {
+    val campus: Campus = when (place.first()) {
+        '#' -> Campus.YEONGEON
+        '*' -> Campus.PYEONGCHANG
+        else -> Campus.GWANAK
+    }
+
+    val placeWithOutCampus = place.removePrefix("#").removePrefix("*")
+    val splits = placeWithOutCampus.split("-").filter { !it.matches("^[A-Za-z]*$".toRegex()) }
+
+    val buildingNumber = when (splits.count()) {
+        3 -> if (splits[1].count() == 1) splits.dropLast(1).joinToString("-") else splits.first()
+        else -> splits.firstOrNull()
+    }?.let {
+        it.trimStart { it == '0' }
+    } ?: return null
+
+    return PlaceInfo(place, campus, buildingNumber)
+}
