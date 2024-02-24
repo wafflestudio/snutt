ff3c6c36de267ac5a50b4608d6d12348dfd44a27
Davin Byeon <bdv111@snu.ac.kr>
2024-02-24 17:33:08 +0900

    Update PopupService.ts (#331)

--- src/core/popup/PopupService.ts
+++ src/core/popup/PopupService.ts
@@ -2,6 +2,12 @@ import User from '@app/core/user/model/User';
 
 export async function getPopups(user: User, osType?: string, osVersion?: string, appType?: string, appVersion?: string) {
   return {
-    content: [],
+    content: [
+       {
+         "key": "new-service-custom-theme",
+         "image_url": "https://snutt-asset.s3.ap-northeast-2.amazonaws.com/popup-images/theme_popup2.png",
+         "hidden_days": 1,
+       },
+     ],
   };
 }
