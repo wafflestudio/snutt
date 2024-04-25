7c6f62303853eec92e85aa763355f3061cbcb9e6
Hank-Choi <zlzlqlzl1@naver.com>
2024-04-25 13:53:05 +0900

    snutt-ev api camelCase로 변경

--- core/src/main/kotlin/evaluation/dto/SnuttEvLectureSummaryDto.kt
+++ core/src/main/kotlin/evaluation/dto/SnuttEvLectureSummaryDto.kt
@@ -1,10 +1,7 @@
 package com.wafflestudio.snu4t.evaluation.dto
 
 import com.fasterxml.jackson.annotation.JsonProperty
-import com.fasterxml.jackson.databind.PropertyNamingStrategies
-import com.fasterxml.jackson.databind.annotation.JsonNaming
 
-@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy::class)
 data class SnuttEvLectureSummaryDto(
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     val snuttId: String,
