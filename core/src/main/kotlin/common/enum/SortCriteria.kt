9c19f89b5b5455a9888f1e1795b654e0e98cb4ea
Chanyeong Lim <dlacksdud2@snu.ac.kr>
2024-07-16 21:34:44 +0900

    SearchQueryLegacy에서 sortCriteria: RATING 으로 받게 변경

--- core/src/main/kotlin/common/enum/SortCriteria.kt
+++ core/src/main/kotlin/common/enum/SortCriteria.kt
@@ -9,12 +9,13 @@ enum class SortCriteria(
     val fullName: String
 ) {
     ID(1, "기본값"),
-    RATING(2, "강의 평점");
+    RATING(2, "평점");
 
     companion object {
         fun getSort(sortCriteria: SortCriteria?): Sort = when (sortCriteria) {
-            RATING -> Sort.by("evInfo.rating").descending()
+            RATING -> Sort.by("evInfo.avgRating").descending()
             else -> Sort.unsorted()
         }
+        fun from(name: String?) = SortCriteria.values().find { it.name == name }
     }
 }
