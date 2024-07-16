086e27b9716f78dcdd5f930d1713a35c0f6308eb
Chanyeong Lim <dlacksdud2@snu.ac.kr>
2024-07-16 21:14:26 +0900

    rating 개수 변수명 count로 수정

--- core/src/main/kotlin/lectures/data/EvInfo.kt
+++ core/src/main/kotlin/lectures/data/EvInfo.kt
@@ -3,5 +3,5 @@ package com.wafflestudio.snu4t.lectures.data
 data class EvInfo(
     val evId: String? = null,
     val avgRating: Double = 0.0,
-    val countRating: Int = 0,
+    val count: Int = 0,
 )
