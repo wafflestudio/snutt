9797e9d6c4b5716d0a029b83aaaa8209598396d1
Chanyeong Lim <dlacksdud2@snu.ac.kr>
2024-07-16 21:15:40 +0900

    evInfo 값들 nullable하게 변경

--- core/src/main/kotlin/lectures/data/EvInfo.kt
+++ core/src/main/kotlin/lectures/data/EvInfo.kt
@@ -2,6 +2,6 @@ package com.wafflestudio.snu4t.lectures.data
 
 data class EvInfo(
     val evId: String? = null,
-    val avgRating: Double = 0.0,
-    val count: Int = 0,
+    val avgRating: Double?,
+    val count: Int?,
 )
