f7a09ed9679a9fe54a533ba9fb0d4a39525d9d25
Chanyeong Lim <dlacksdud2@snu.ac.kr>
2024-08-10 18:31:42 +0900

    tags api 마이그레이션

---
+++ core/src/main/kotlin/tag/data/TagListResponse.kt
@@ -0,0 +1,24 @@
+package com.wafflestudio.snu4t.tag.data
+
+import com.fasterxml.jackson.annotation.JsonProperty
+
+data class TagListResponse(
+    val classification: List<String>,
+    val department: List<String>,
+    @JsonProperty("academic_year")
+    val academicYear: List<String>,
+    val credit: List<String>,
+    val instructor: List<String>,
+    val category: List<String>,
+    @JsonProperty("updated_at")
+    val updatedAt: Long
+)
+fun TagListResponse(tagList: TagList) = TagListResponse(
+    classification = tagList.tagCollection.classification,
+    department = tagList.tagCollection.department,
+    academicYear = tagList.tagCollection.academicYear,
+    credit = tagList.tagCollection.credit,
+    instructor = tagList.tagCollection.instructor,
+    category = tagList.tagCollection.category,
+    updatedAt = tagList.updatedAt.toEpochMilli()
+)
