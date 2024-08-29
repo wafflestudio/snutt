55bdb898a76993b0f4464e33f384df0cf6f632e4
Chanyeong Lim <dlacksdud2@gmail.com>
2024-08-29 13:54:35 +0900

    firebase-admin 9.3.0 upgrade

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -8,7 +8,7 @@ dependencies {
     implementation("software.amazon.awssdk:secretsmanager:2.25.15")
     implementation("software.amazon.awssdk:sts:2.25.15")
     implementation("software.amazon.awssdk:ses:2.25.15")
-    implementation("com.google.firebase:firebase-admin:9.1.1")
+    implementation("com.google.firebase:firebase-admin:9.3.0")
 
     testFixturesImplementation("org.testcontainers:mongodb:1.19.0")
     testFixturesImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
