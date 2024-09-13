383389833df0215ddf46d7709a6ca9bf945b5430
Hank-Choi <zlzlqlzl1@naver.com>
2024-09-13 22:54:12 +0900

    api docs 추가

--- api/src/main/kotlin/router/docs/ThemeDocs.kt
+++ api/src/main/kotlin/router/docs/ThemeDocs.kt
@@ -1,7 +1,10 @@
 package com.wafflestudio.snu4t.router.docs
 
+import com.wafflestudio.snu4t.common.dto.ListResponse
 import com.wafflestudio.snu4t.timetables.dto.TimetableThemeDto
 import com.wafflestudio.snu4t.timetables.dto.request.TimetableThemeAddRequestDto
+import com.wafflestudio.snu4t.timetables.dto.request.TimetableThemeDownloadRequestDto
+import com.wafflestudio.snu4t.timetables.dto.request.TimetableThemePublishRequestDto
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.enums.ParameterIn
@@ -31,6 +34,21 @@ import org.springframework.web.bind.annotation.RequestMethod
                     ),
                 ],
             ),
+    ),
+    RouterOperation(
+        path = "/v1/themes/best", method = [RequestMethod.GET], produces = [MediaType.APPLICATION_JSON_VALUE],
+        operation = Operation(
+            operationId = "getBestThemes",
+            parameters = [Parameter(`in` = ParameterIn.QUERY, name = "page", required = true)],
+            responses = [ApiResponse(responseCode = "200", content = [Content(schema = Schema(implementation = ThemesResponse::class))])],
+        ),
+    ),
+    RouterOperation(
+        path = "/v1/themes/friends", method = [RequestMethod.GET], produces = [MediaType.APPLICATION_JSON_VALUE],
+        operation = Operation(
+            operationId = "getFriendsThemes",
+            responses = [ApiResponse(responseCode = "200", content = [Content(schema = Schema(implementation = ThemesResponse::class))])],
+        ),
     ),
     RouterOperation(
         path = "/v1/themes",
@@ -127,6 +145,38 @@ import org.springframework.web.bind.annotation.RequestMethod
             ),
     ),
     RouterOperation(
+        path = "/v1/themes/{themeId}/publish", method = [RequestMethod.POST], produces = [MediaType.APPLICATION_JSON_VALUE],
+        operation = Operation(
+            operationId = "publishTheme",
+            parameters = [Parameter(`in` = ParameterIn.PATH, name = "themeId", required = true)],
+            requestBody = RequestBody(
+                content = [Content(schema = Schema(implementation = TimetableThemePublishRequestDto::class))],
+                required = true,
+            ),
+            responses = [ApiResponse(responseCode = "200", content = [Content(schema = Schema())])],
+        ),
+    ),
+    RouterOperation(
+        path = "/v1/themes/{themeId}/download", method = [RequestMethod.POST], produces = [MediaType.APPLICATION_JSON_VALUE],
+        operation = Operation(
+            operationId = "downloadTheme",
+            parameters = [Parameter(`in` = ParameterIn.PATH, name = "themeId", required = true)],
+            requestBody = RequestBody(
+                content = [Content(schema = Schema(implementation = TimetableThemeDownloadRequestDto::class))],
+                required = true,
+            ),
+            responses = [ApiResponse(responseCode = "200", content = [Content(schema = Schema(implementation = TimetableThemeDto::class))])],
+        ),
+    ),
+    RouterOperation(
+        path = "/v1/themes/search", method = [RequestMethod.GET], produces = [MediaType.APPLICATION_JSON_VALUE],
+        operation = Operation(
+            operationId = "searchThemes",
+            parameters = [Parameter(`in` = ParameterIn.QUERY, name = "query", required = true)],
+            responses = [ApiResponse(responseCode = "200", content = [Content(array = ArraySchema(schema = Schema(implementation = ThemesResponse::class)))]),],
+        ),
+    ),
+    RouterOperation(
         path = "/v1/themes/basic/{basicThemeTypeValue}/default",
         method = [RequestMethod.POST],
         produces = [MediaType.APPLICATION_JSON_VALUE],
@@ -176,3 +226,5 @@ import org.springframework.web.bind.annotation.RequestMethod
     ),
 )
 annotation class ThemeDocs
+
+private class ThemesResponse : ListResponse<TimetableThemeDto>(listOf())
