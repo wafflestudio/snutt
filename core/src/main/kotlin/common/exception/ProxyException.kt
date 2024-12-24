04a15fd41232f02c09602f0d2d785f97efc12eae
Chanyeong Lim <dlacksdud2@gmail.com>
2024-12-24 10:46:07 +0900

    serialize error body

--- core/src/main/kotlin/common/exception/ProxyException.kt
+++ core/src/main/kotlin/common/exception/ProxyException.kt
@@ -1,8 +1,9 @@
 package com.wafflestudio.snu4t.common.exception
 
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.springframework.http.HttpStatusCode
 
 class ProxyException(
     val statusCode: HttpStatusCode,
     val errorBody: Map<String, Any?>,
-) : RuntimeException(errorBody.toString())
+) : RuntimeException(ObjectMapper().writeValueAsString(errorBody))
