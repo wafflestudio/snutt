ca95dd6dcecb73defc52b3fb5bfd13c1cde793d6
Chanyeong Lim <dlacksdud2@gmail.com>
2025-01-09 12:37:24 +0900

    remove snake case

--- core/src/main/kotlin/evaluation/dto/EvUserDto.kt
+++ core/src/main/kotlin/evaluation/dto/EvUserDto.kt
@@ -1,16 +1,18 @@
 package com.wafflestudio.snu4t.evaluation.dto
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.wafflestudio.snu4t.users.data.User
 
 data class EvUserDto(
     val id: String?,
     val email: String?,
-    val local_id: String?,
+    @JsonProperty("local_id")
+    val localId: String?,
 )
 
 fun EvUserDto(user: User) =
     EvUserDto(
         id = user.id,
         email = user.email,
-        local_id = user.credential.localId,
+        localId = user.credential.localId,
     )
