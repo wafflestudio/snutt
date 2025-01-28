e96fa91232dc69a687f8007e6eb2842f2ec99ce4
Chanyeong Lim <dlacksdud2@gmail.com>
2025-01-28 14:39:36 +0900

    LocalDateTime과 ZonedDateTime에서 항상 밀리초 부분 보이게 수정

---
+++ core/src/main/kotlin/config/JacksonConfig.kt
@@ -0,0 +1,50 @@
+package com.wafflestudio.snu4t.config
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.databind.SerializationFeature
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer
+import com.fasterxml.jackson.datatype.jsr310.ser.ZonedDateTimeSerializer
+import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import java.time.LocalDateTime
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.time.format.DateTimeFormatterBuilder
+import java.time.temporal.ChronoField
+
+@Configuration
+class JacksonConfig {
+    @Bean
+    fun objectMapper(): ObjectMapper {
+        val timeFormatter =
+            DateTimeFormatterBuilder()
+                .append(DateTimeFormatter.ISO_LOCAL_DATE)
+                .appendLiteral('T')
+                .appendValue(ChronoField.HOUR_OF_DAY, 2)
+                .appendLiteral(':')
+                .appendValue(ChronoField.MINUTE_OF_HOUR, 2)
+                .appendLiteral(':')
+                .appendValue(ChronoField.SECOND_OF_MINUTE, 2)
+                .appendFraction(ChronoField.NANO_OF_SECOND, 3, 3, true)
+                .toFormatter()
+
+        val javaTimeModule =
+            JavaTimeModule().apply {
+                addSerializer(
+                    ZonedDateTime::class.java,
+                    ZonedDateTimeSerializer(timeFormatter),
+                )
+                addSerializer(
+                    LocalDateTime::class.java,
+                    LocalDateTimeSerializer(timeFormatter),
+                )
+            }
+
+        return jacksonObjectMapper().apply {
+            registerModule(javaTimeModule)
+            disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
+        }
+    }
+}
