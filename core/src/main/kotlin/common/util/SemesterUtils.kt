ff5966b8f8c678db5d2631501b9aa9ffa5799b26
eastshine2741 <eastshine@snu.ac.kr>
2025-07-25 01:02:10 +0900

    임시커밋

--- core/src/main/kotlin/common/util/SemesterUtils.kt
+++ core/src/main/kotlin/common/util/SemesterUtils.kt
@@ -5,28 +5,31 @@ import java.time.LocalDate
 import java.time.MonthDay
 
 object SemesterUtils {
+    private val dateToSemester = mapOf(
+        MonthDay.of(1, 1)..MonthDay.of(1, 31) to (Semester.WINTER to -1),
+        MonthDay.of(3, 2)..MonthDay.of(6, 23) to (Semester.SPRING to 0),
+        MonthDay.of(6, 24)..MonthDay.of(8, 7) to (Semester.SUMMER to 0),
+        MonthDay.of(9, 1)..MonthDay.of(12, 19) to (Semester.AUTUMN to 0),
+        MonthDay.of(12, 20)..MonthDay.of(12, 31) to (Semester.WINTER to 0)
+    )
+
     fun getCurrentYearAndSemester(): Pair<Int, Semester>? {
         val now = LocalDate.now()
         val currentMonthDay = MonthDay.from(now)
-        return when {
-            currentMonthDay <= MonthDay.of(1, 31) -> {
-                now.year - 1 to Semester.WINTER
+
+        val (semester, yearOffset) = dateToSemester.entries.firstOrNull { currentMonthDay in it.key }?.value
+            ?: return null
+
+        return (now.year + yearOffset) to semester
+    }
+
+    fun getCurrentOrNextYearAndSemester(): Pair<Int, Semester> {
+        val now = LocalDate.now()
+        val currentMonthDay = MonthDay.from(now)
+
+        return getCurrentYearAndSemester()
+            ?: dateToSemester.entries.firstOrNull { currentMonthDay < it.key.start }!!.value.let { (semester, yearOffset) ->
+                (now.year + yearOffset) to semester
             }
-            currentMonthDay in MonthDay.of(3, 2)..MonthDay.of(6, 23) -> {
-                now.year to Semester.SPRING
-            }
-            currentMonthDay in MonthDay.of(6, 24)..MonthDay.of(8, 7) -> {
-                now.year to Semester.SUMMER
-            }
-            currentMonthDay in MonthDay.of(9, 1)..MonthDay.of(12, 19) -> {
-                now.year to Semester.AUTUMN
-            }
-            currentMonthDay >= MonthDay.of(12, 20) -> {
-                now.year to Semester.WINTER
-            }
-            else -> {
-                null // No semester matches the current date
-            }
-        }
     }
 }
