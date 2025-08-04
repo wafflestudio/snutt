0f9e17929e0312fe8f9804ee97dd8277f71c2295
Chanyeong Lim <dlacksdud2@gmail.com>
2025-08-04 09:53:27 +0900

    update gradle and project dependencies

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -4,13 +4,15 @@ plugins {
 
 dependencies {
     implementation("org.springframework.security:spring-security-crypto")
-    implementation("software.amazon.awssdk:s3:2.25.15")
-    implementation("software.amazon.awssdk:ses:2.25.15")
+    implementation("software.amazon.awssdk:s3:2.32.14")
+    implementation("software.amazon.awssdk:ses:2.32.14")
     implementation("com.wafflestudio.spring:spring-boot-starter-waffle:1.0.4")
-    implementation("com.google.firebase:firebase-admin:9.3.0")
-    implementation("io.jsonwebtoken:jjwt:0.9.1")
+    implementation("com.google.firebase:firebase-admin:9.5.0")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
 
-    testFixturesImplementation("org.testcontainers:mongodb:1.19.0")
+    testFixturesImplementation("org.testcontainers:mongodb:1.21.3")
     testFixturesImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
     testFixturesImplementation("org.springframework.boot:spring-boot-starter-test")
     testFixturesImplementation("org.springframework.boot:spring-boot-starter-webflux")
