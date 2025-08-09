63796e4ac474f9106813886d159cff74e5d60dae
eastshine2741 <eastshine@snu.ac.kr>
2025-08-10 01:53:21 +0900

    푸시알림 테스트를 위해 prod 외 환경에서는 학기 range에 빈 구간 없애기

--- core/src/main/kotlin/common/util/SemesterUtils.kt
+++ core/src/main/kotlin/common/util/SemesterUtils.kt
@@ -1,6 +1,7 @@
 package com.wafflestudio.snutt.common.util
 
 import com.wafflestudio.snutt.common.enum.Semester
+import com.wafflestudio.snutt.config.PhaseUtils
 import java.time.Instant
 import java.time.MonthDay
 import java.time.ZoneId
@@ -21,11 +22,27 @@ object SemesterUtils {
             SemesterInfo(Semester.WINTER, 0, MonthDay.of(12, 20)..MonthDay.of(12, 31)),
         )
 
+    private val semesterPeriodsForTest =
+        listOf(
+            SemesterInfo(Semester.WINTER, -1, MonthDay.of(1, 1)..MonthDay.of(1, 31)),
+            SemesterInfo(Semester.SPRING, 0, MonthDay.of(2, 1)..MonthDay.of(6, 23)), // 알림 테스트를 위해 빈 기간을 두지 않는다.
+            SemesterInfo(Semester.SUMMER, 0, MonthDay.of(6, 24)..MonthDay.of(8, 7)),
+            SemesterInfo(Semester.AUTUMN, 0, MonthDay.of(8, 8)..MonthDay.of(12, 19)), // 알림 테스트를 위해 빈 기간을 두지 않는다.
+            SemesterInfo(Semester.WINTER, 0, MonthDay.of(12, 20)..MonthDay.of(12, 31)),
+        )
+
+    private fun getSemesterPeriods(): List<SemesterInfo> =
+        if (PhaseUtils.getPhase().isProd) {
+            semesterPeriods
+        } else {
+            semesterPeriodsForTest
+        }
+
     fun getCurrentYearAndSemester(now: Instant): Pair<Int, Semester>? {
         val localDate = now.atZone(ZoneId.of("Asia/Seoul")).toLocalDate()
         val currentMonthDay = MonthDay.from(localDate)
         val currentPeriod =
-            semesterPeriods.firstOrNull { currentMonthDay in it.dateRange }
+            getSemesterPeriods().firstOrNull { currentMonthDay in it.dateRange }
                 ?: return null
 
         return localDate.year + currentPeriod.academicYearOffset to currentPeriod.semester
@@ -35,7 +52,7 @@ object SemesterUtils {
         getCurrentYearAndSemester(now) ?: run {
             val localDate = now.atZone(ZoneId.of("Asia/Seoul")).toLocalDate()
             val currentMonthDay = MonthDay.from(localDate)
-            semesterPeriods.first { currentMonthDay < it.dateRange.start }.let { nextPeriod ->
+            getSemesterPeriods().first { currentMonthDay < it.dateRange.start }.let { nextPeriod ->
                 (localDate.year + nextPeriod.academicYearOffset) to nextPeriod.semester
             }
         }
