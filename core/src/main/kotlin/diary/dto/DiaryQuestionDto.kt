bffb52e2f9a84e2ae20642d04e4a743cfb102be8
Chanyeong Lim <asp345@posteo.net>
2025-09-08 21:08:50 +0900

    short question/answers DiaryQuestionDto에서 제거

--- core/src/main/kotlin/diary/dto/DiaryQuestionDto.kt
+++ core/src/main/kotlin/diary/dto/DiaryQuestionDto.kt
@@ -5,16 +5,12 @@ import com.wafflestudio.snutt.diary.data.DiaryQuestion
 data class DiaryQuestionDto(
     val id: String,
     val question: String,
-    val shortQuestion: String,
     val answers: List<String>,
-    val shortAnswers: List<String>,
 )
 
 fun DiaryQuestionDto(diaryQuestion: DiaryQuestion) =
     DiaryQuestionDto(
         id = diaryQuestion.id!!,
         question = diaryQuestion.question,
-        shortQuestion = diaryQuestion.shortQuestion,
         answers = diaryQuestion.answers,
-        shortAnswers = diaryQuestion.shortAnswers,
     )
