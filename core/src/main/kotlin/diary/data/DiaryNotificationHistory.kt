c008a8d9b4fc44955c1f1308400f735709924fc9
asp345 <dlacksdud2@gmail.com>
2025-09-24 14:24:09 +0900

    add Document annotation

--- core/src/main/kotlin/diary/data/DiaryNotificationHistory.kt
+++ core/src/main/kotlin/diary/data/DiaryNotificationHistory.kt
@@ -1,9 +1,11 @@
 package com.wafflestudio.snutt.diary.data
 
+import org.springframework.data.mongodb.core.mapping.Document
 import org.springframework.data.mongodb.core.mapping.Field
 import org.springframework.data.mongodb.core.mapping.FieldType
 import java.time.Instant
 
+@Document
 data class DiaryNotificationHistory(
     @Field(targetType = FieldType.OBJECT_ID)
     val userId: String,
