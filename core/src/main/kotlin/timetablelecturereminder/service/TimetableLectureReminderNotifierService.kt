558f5bce47e99338a9490d0dd314ea92dae246d9
Dongyeop Song <eastshine@snu.ac.kr>
2025-10-29 23:57:18 +0900

    [강의 리마인더] 푸시 문구 수정 (#414)

--- core/src/main/kotlin/timetablelecturereminder/service/TimetableLectureReminderNotifierService.kt
+++ core/src/main/kotlin/timetablelecturereminder/service/TimetableLectureReminderNotifierService.kt
@@ -42,7 +42,7 @@ class TimetableLectureReminderNotifierServiceImpl(
         val lockKey = CacheKey.LOCK_SEND_TIMETABLE_LECTURE_REMINDER_NOTIFICATION.build()
         cache.withLock(lockKey) {
             try {
-                logger.info("강의 리마인더 알림 전송 작업을 시작합니다.")
+                logger.debug("강의 리마인더 알림 전송 작업을 시작합니다.")
                 val currentTime = Instant.now()
                 val (currentYear, currentSemester) =
                     semesterService.getCurrentYearAndSemester(currentTime) ?: run {
@@ -208,14 +208,12 @@ class TimetableLectureReminderNotifierServiceImpl(
         val timetableLecture =
             timetableAndReminder.timetable.lectures.find { it.id == timetableAndReminder.reminder.timetableLectureId } ?: return null
 
-        val pushTitleType = if (timetableLecture.lectureId == null) "일정" else "강의"
-        val pushBodyType = if (timetableLecture.lectureId == null) "" else "강의"
-        val pushTitle = "\uD83D\uDCDA $pushTitleType 리마인더"
+        val pushTitle = "\uD83D\uDCDA 강의 리마인더"
         val pushBody =
             when {
-                offsetMinutes == 0 -> "${timetableLecture.courseTitle} $pushBodyType 시간이에요."
-                offsetMinutes > 0 -> "${timetableLecture.courseTitle} $pushBodyType 시작 ${offsetMinutes}분 후예요."
-                else -> "${timetableLecture.courseTitle} $pushBodyType 시작 ${-offsetMinutes}분 전이에요."
+                offsetMinutes == 0 -> "${timetableLecture.courseTitle} 강의 시간이에요."
+                offsetMinutes > 0 -> "${timetableLecture.courseTitle} 강의 시작 ${offsetMinutes}분 후예요."
+                else -> "${timetableLecture.courseTitle} 강의 시작 ${-offsetMinutes}분 전이에요."
             }
 
         return PushMessage(
