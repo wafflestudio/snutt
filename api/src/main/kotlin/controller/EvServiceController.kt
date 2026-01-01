2fb73f969ebf68ec716518b4d7b553224abc6b96
Hank-Choi <zlzlqlzl1@naver.com>
2026-01-01 20:50:16 +0900

    ev 서비스 관련 dto reflection 등록

--- api/src/main/kotlin/controller/EvServiceController.kt
+++ api/src/main/kotlin/controller/EvServiceController.kt
@@ -1,9 +1,16 @@
 package com.wafflestudio.snutt.controller
 
+import com.wafflestudio.snutt.common.dto.ListResponse
 import com.wafflestudio.snutt.config.CurrentUser
+import com.wafflestudio.snutt.evaluation.dto.EvErrorResponse
+import com.wafflestudio.snutt.evaluation.dto.EvLectureInfoDto
+import com.wafflestudio.snutt.evaluation.dto.EvUserDto
+import com.wafflestudio.snutt.evaluation.dto.SnuttEvLectureIdDto
+import com.wafflestudio.snutt.evaluation.dto.SnuttEvLectureSummaryDto
 import com.wafflestudio.snutt.evaluation.service.EvService
 import com.wafflestudio.snutt.filter.SnuttDefaultApiFilterTarget
 import com.wafflestudio.snutt.users.data.User
+import org.springframework.aot.hint.annotation.RegisterReflectionForBinding
 import org.springframework.http.HttpMethod
 import org.springframework.util.MultiValueMap
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -18,6 +25,14 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @SnuttDefaultApiFilterTarget
+@RegisterReflectionForBinding(
+    EvLectureInfoDto::class,
+    EvUserDto::class,
+    EvErrorResponse::class,
+    SnuttEvLectureIdDto::class,
+    SnuttEvLectureSummaryDto::class,
+    ListResponse::class,
+)
 @RequestMapping("/v1/ev-service", "/ev-service")
 class EvServiceController(
     private val evService: EvService,
