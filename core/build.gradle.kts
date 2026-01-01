4e591358efbbcbb2b1a9eb821600bdc1f745dfe4
Hank-Choi <zlzlqlzl1@naver.com>
2026-01-01 20:50:50 +0900

    firebase 필요 리소스 등록

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -19,7 +19,7 @@ dependencies {
     implementation("software.amazon.awssdk:s3:2.32.14")
     implementation("software.amazon.awssdk:ses:2.32.14")
     implementation("com.wafflestudio.spring:spring-boot-starter-waffle:2.0.0")
-    implementation("com.google.firebase:firebase-admin:9.5.0")
+    implementation("com.google.firebase:firebase-admin:9.7.0")
     implementation("io.jsonwebtoken:jjwt-api:0.13.0")
     implementation("io.jsonwebtoken:jjwt-impl:0.13.0")
     implementation("io.jsonwebtoken:jjwt-jackson:0.13.0")
