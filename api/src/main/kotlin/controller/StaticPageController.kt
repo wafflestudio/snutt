d541d74fcba1843a076fc9ce39b910e8dfc518e6
Hankyeol Choi <zlzlqlzl1@naver.com>
2026-01-01 23:01:27 +0900

    StaticPageController 버그 픽스 (#453)

--- api/src/main/kotlin/controller/StaticPageController.kt
+++ api/src/main/kotlin/controller/StaticPageController.kt
@@ -1,12 +1,12 @@
 package com.wafflestudio.snutt.controller
 
+import org.springframework.core.io.Resource
 import org.springframework.core.io.ResourceLoader
 import org.springframework.http.CacheControl
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
-import org.springframework.web.reactive.function.server.ServerResponse
-import org.springframework.web.reactive.function.server.bodyValueAndAwait
 import java.time.Duration
 
 @RestController
@@ -15,29 +15,29 @@ class StaticPageController(
     private val resourceLoader: ResourceLoader,
 ) {
     companion object {
-        private const val RESOURCE_PATH = "/views"
+        private const val RESOURCE_PATH = "classpath:views"
         private val staticResponse =
-            ServerResponse
+            ResponseEntity
                 .ok()
                 .cacheControl(CacheControl.maxAge(Duration.ofDays(1)))
                 .header("Content-Type", "text/html; charset=utf-8")
     }
 
     @GetMapping("/member", produces = ["text/html; charset=utf-8"])
-    suspend fun member(): ServerResponse =
-        staticResponse.bodyValueAndAwait(
+    suspend fun member(): ResponseEntity<Resource> =
+        staticResponse.body(
             resourceLoader.getResource("$RESOURCE_PATH/member.html"),
         )
 
     @GetMapping("/privacy_policy", produces = ["text/html; charset=utf-8"])
-    suspend fun privacyPolicy(): ServerResponse =
-        staticResponse.bodyValueAndAwait(
+    suspend fun privacyPolicy(): ResponseEntity<Resource> =
+        staticResponse.body(
             resourceLoader.getResource("$RESOURCE_PATH/privacy_policy.html"),
         )
 
     @GetMapping("/terms_of_service", produces = ["text/html; charset=utf-8"])
-    suspend fun termsOfService(): ServerResponse =
-        staticResponse.bodyValueAndAwait(
+    suspend fun termsOfService(): ResponseEntity<Resource> =
+        staticResponse.body(
             resourceLoader.getResource("$RESOURCE_PATH/terms_of_service.html"),
         )
 }
