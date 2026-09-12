2818700465af98f10c105b9a5dc2c2985a7b4721
Chanyeong Lim <dlacksdud2@snu.ac.kr>
2026-09-12 15:46:01 +0900

    Revert "친구 목록 조회 API 요청/응답 진단 로그 추가" (#551)

--- core/src/main/kotlin/debug/repository/FriendListDebugLogRepository.kt
+++
@@ -1,6 +0,0 @@
-package com.wafflestudio.snutt.debug.repository
-
-import com.wafflestudio.snutt.debug.data.FriendListDebugLog
-import org.springframework.data.repository.kotlin.CoroutineCrudRepository
-
-interface FriendListDebugLogRepository : CoroutineCrudRepository<FriendListDebugLog, String>
